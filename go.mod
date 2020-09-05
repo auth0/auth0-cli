50cc4d6b8cc78040001eacd3d47725a09844a47f
Cyril David <cyril.david@auth0.com>
2020-09-04 21:32:43 -0700

    go mod tidy + vendor

--- go.mod
+++ go.mod
@@ -3,31 +3,24 @@ module github.com/auth0/auth0-cli
 go 1.14
 
 require (
-	github.com/BurntSushi/toml v0.3.1
+	github.com/aybabtme/iocontrol v0.0.0-20150809002002-ad15bcfc95a0 // indirect
+	github.com/benbjohnson/clock v1.0.3 // indirect
 	github.com/briandowns/spinner v1.11.1
+	github.com/cyx/auth0 v1.3.1-0.20200903063631-554acea20a54
+	github.com/dustin/go-humanize v1.0.0 // indirect
 	github.com/fatih/color v1.9.0 // indirect
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
+	github.com/golang/protobuf v1.3.2 // indirect
 	github.com/logrusorgru/aurora v2.0.3+incompatible
 	github.com/mattn/go-colorable v0.1.6 // indirect
-	github.com/mgutz/ansi v0.0.0-20170206155736-9520e82c474b // indirect
-	github.com/mitchellh/go-homedir v1.1.0
-	github.com/mitchellh/mapstructure v1.3.0 // indirect
-	github.com/onsi/ginkgo v1.12.0 // indirect
-	github.com/onsi/gomega v1.9.0 // indirect
-	github.com/pelletier/go-toml v1.7.0 // indirect
-	github.com/sirupsen/logrus v1.6.0
 	github.com/spf13/afero v1.2.2
-	github.com/spf13/cast v1.3.1 // indirect
 	github.com/spf13/cobra v1.0.0
-	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5
-	github.com/spf13/viper v1.7.1
 	github.com/stretchr/testify v1.5.1
 	github.com/tidwall/pretty v1.0.2
-	github.com/x-cray/logrus-prefixed-formatter v0.5.2
 	golang.org/x/crypto v0.0.0-20200429183012-4b2356b1ed79
 	golang.org/x/net v0.0.0-20200501053045-e0ff5e5a1de5 // indirect
 	golang.org/x/sys v0.0.0-20200501145240-bc7a7d42d5c3 // indirect
+	google.golang.org/appengine v1.6.1 // indirect
 	gopkg.in/check.v1 v1.0.0-20190902080502-41f04d3bba15 // indirect
-	gopkg.in/ini.v1 v1.55.0 // indirect
+	gopkg.in/yaml.v2 v2.2.8 // indirect
 )
---
+++ vendor/github.com/cyx/auth0/management/actions.go
@@ -0,0 +1,191 @@
+package management
+
+import (
+	"context"
+	"net/url"
+	"time"
+)
+
+type Action struct {
+	ID                string    `json:"id,omitempty"`
+	Name              string    `json:"name,omitempty"`
+	SupportedTriggers []Trigger `json:"supported_triggers,omitempty"`
+	// TODO: add required configuration / secrets
+	// TODO: maybe add created_at / updated_at
+}
+
+type VersionStatus string
+
+const (
+	VersionStatusPending  VersionStatus = "pending"
+	VersionStatusRetrying VersionStatus = "retrying"
+	VersionStatusBuilding VersionStatus = "building"
+	VersionStatusBuilt    VersionStatus = "built"
+
+	// TODO(cyx): maybe get rid of this
+	VersionStatusPromoted VersionStatus = "promoted"
+)
+
+type TriggerID string
+
+const (
+	PostLogin         TriggerID = "post-login"
+	ClientCredentials TriggerID = "client-credentials"
+)
+
+type ActionVersion struct {
+	ID           string        `json:"id,omitempty"`
+	Action       *Action       `json:"action,omitempty"`
+	Code         string        `json:"code"`
+	Dependencies []Dependency  `json:"dependencies,omitempty"`
+	Runtime      string        `json:"runtime,omitempty"`
+	Status       VersionStatus `json:"status,omitempty"`
+	Number       int           `json:"number,omitempty"`
+
+	// TODO: maybe add errors?
+	// TODO: add timestamps?
+}
+
+type Dependency struct {
+	Name        string `json:"name"`
+	Version     string `json:"version"`
+	RegistryURL string `json:"registry_url"`
+}
+
+type Trigger struct {
+	ID      TriggerID `json:"id"`
+	Version string    `json:"version"`
+	URL     string    `json:"url"`
+}
+
+type ActionList struct {
+	List
+	Actions []*Action `json:"actions"`
+}
+
+type ActionVersionList struct {
+	List
+	Versions []*ActionVersion `json:"versions"`
+}
+
+type Object map[string]interface{}
+
+type ActionManager struct {
+	*Management
+}
+
+func (m *ActionManager) Create(a *Action) error {
+	return m.post(m.uri("actions", "actions"), a)
+}
+
+func (m *ActionManager) Read(id string) (*Action, error) {
+	var a Action
+	err := m.get(m.uri("actions", "actions", id), &a)
+	return &a, err
+}
+
+func (m *ActionManager) Update(id string, a *Action) error {
+	// We'll get a 400 if we try to send the ID as part of the payload.
+	a.ID = ""
+	return m.patch(m.uri("actions", "actions", id), a)
+}
+
+func (m *ActionManager) Delete(id string) error {
+	return m.delete(m.uri("actions", "actions", id))
+}
+
+func WithTriggerID(id TriggerID) ListOption {
+	return func(v url.Values) {
+		v.Set("triggerId", string(id))
+	}
+}
+
+// TODO(cyx): do the standard m.q(opts) here supporting per_page, etc.
+func (m *ActionManager) List(opts ...ListOption) (*ActionList, error) {
+	var list ActionList
+	err := m.get(m.uri("actions", "actions")+m.q(opts), &list)
+	return &list, err
+}
+
+type ActionVersionManager struct {
+	*Management
+}
+
+func (m *ActionVersionManager) Deploy(actionID string, v *ActionVersion) error {
+	if err := m.post(m.uri("actions", "actions", actionID, "versions"), v); err != nil {
+		return err
+	}
+
+	// Wait up to 1 minute for deploying an action version.
+	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
+	defer cancel()
+
+	ticker := time.NewTicker(5 * time.Second)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+
+		case <-ticker.C:
+			got, err := m.Read(actionID, v.ID)
+			if err != nil {
+				return err
+			}
+
+			if got.Status == VersionStatusBuilt {
+				if _, err := m.Promote(actionID, got.ID); err != nil {
+					return err
+				}
+
+				// Refresh the final representation of this
+				// action version.
+				*v = *got
+				return nil
+			}
+		}
+	}
+}
+
+func (m *ActionVersionManager) Create(actionID string, v *ActionVersion) error {
+	return m.post(m.uri("actions", "actions", actionID, "versions"), v)
+}
+
+// TODO(cyx): This isn't implemented yet.
+func (m *ActionVersionManager) Update(actionID string, v *ActionVersion) error {
+	return m.patch(m.uri("actions", "actions", actionID, "versions", "draft"), v)
+}
+
+func (m *ActionVersionManager) Read(actionID, id string) (*ActionVersion, error) {
+	var v ActionVersion
+	err := m.get(m.uri("actions", "actions", actionID, "versions", id), &v)
+	return &v, err
+}
+
+func (m *ActionVersionManager) Delete(actionID, id string) error {
+	return m.delete(m.uri("actions", "actions", actionID, "versions", id))
+}
+
+func (m *ActionVersionManager) List(actionID string, opts ...ListOption) (*ActionVersionList, error) {
+	var list ActionVersionList
+	opts = m.defaults(opts)
+	err := m.get(m.uri("actions", "actions", actionID, "versions")+m.q(opts), &list)
+	return &list, err
+}
+
+// TODO(cyx): might call this `activate` instead later. Still fleshing out the
+// name.
+func (m *ActionVersionManager) Promote(actionID, id string) (*ActionVersion, error) {
+	var v ActionVersion
+	err := m.post(m.uri("actions", "actions", actionID, "versions", id, "promote"), &v)
+	return &v, err
+}
+
+// TODO(cyx): consider how the `draft` test looks like. Will it just use
+// `draft` in place of the ID?
+func (m *ActionVersionManager) Test(actionID, id string, payload Object) (Object, error) {
+	v := Object{"payload": payload}
+	err := m.post(m.uri("actions", "actions", actionID, "versions", id, "test"), &v)
+	return v, err
+}
