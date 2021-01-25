6211e87f8dfe18cd270e613fc74b36af94295918
Danny Turcotte <danny.turcotte@auth0.com>
2021-01-24 21:50:52 -0500

    Boilerplate

---
+++ internal/cli/actions.go
@@ -0,0 +1,40 @@
+package cli
+
+import (
+	"github.com/spf13/cobra"
+)
+
+func actionsCmd(cli *cli) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "actions",
+		Short: "manage resources for actions.",
+	}
+
+	cmd.SetUsageTemplate(resourceUsageTemplate())
+	cmd.AddCommand(listActionsCmd(cli))
+
+	return cmd
+}
+
+func listActionsCmd(cli *cli) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:   "list",
+		Short: "Lists your existing actions",
+		Long: `$ auth0 actions list
+Lists your existing actions. To create one try:
+
+    $ auth0 actions create
+`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			list, err := cli.api.Client.List()
+			if err != nil {
+				return err
+			}
+
+			cli.renderer.ClientList(list.Clients)
+			return nil
+		},
+	}
+
+	return cmd
+}
