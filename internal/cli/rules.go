bf4a14cecc392b2e4e23ae5d016f773514a972e9
Alexis Kulash <alexis.kulash@auth0.com>
2021-01-25 14:34:26 -0500

    WIP: Adding initial base for a new "create rule" subcommand

--- internal/cli/rules.go
+++ internal/cli/rules.go
@@ -4,6 +4,9 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var name, script, order string
+var enable bool
+
 func rulesCmd(cli *cli) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "rules",
@@ -12,6 +15,8 @@ func rulesCmd(cli *cli) *cobra.Command {
 
 	cmd.SetUsageTemplate(resourceUsageTemplate())
 	cmd.AddCommand(listRulesCmd(cli))
+	cmd.AddCommand(createRulesCmd(cli))
+
 	return cmd
 }
 
@@ -31,6 +36,36 @@ func listRulesCmd(cli *cli) *cobra.Command {
 			return nil
 		},
 	}
+
+	return cmd
+}
+
+func createRulesCmd(cli *cli) *cobra.Command {
+	cmd := &cobra.Command{
+		Use:              "create [OPTIONS]",
+		TraverseChildren: true,
+		Short:            "Create a new rule",
+		Long:             "Create a new rule in your current tenant.",
+		Args:             cobra.MinimumNArgs(2),
+		RunE: func(cmd *cobra.Command, args []string) error {
+			// TODO: Add the proper functionality here -- currently not working
+			err := cli.api.Client.Rule.Create()
+
+			if err != nil {
+				return err
+			}
+
+			cli.renderer.Infof("Your rule `%s` was successfully created.", args[0])
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVarP(&name, "name", "n", "", "Name of this rule (required)")
+	cmd.Flags().StringVarP(&script, "script", "s", "", "Code to be executed when this rule runs (required)")
+	cmd.Flags().StringVarP(&order, "order", "o", "", "Order that this rule should execute in relative to other rules. Lower-valued rules execute first.")
+	cmd.Flags().BoolVarP(&enable, "enable", "e", false, "Whether the rule is enabled (true), or disabled (false).")
+	cmd.MarkFlagRequired("name")
+	cmd.MarkFlagRequired("script")
 
 	return cmd
 }
