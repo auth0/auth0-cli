04a86b75a66dc53f6f1202921dcb62cf511c0a78
Alexis Kulash <alexis.kulash@auth0.com>
2021-01-25 15:44:20 -0500

    Adding onto MGMT API functionality

--- internal/cli/rules.go
+++ internal/cli/rules.go
@@ -2,10 +2,12 @@ package cli
 
 import (
 	"github.com/spf13/cobra"
+	"gopkg.in/auth0.v5/management"
 )
 
-var name, script, order string
-var enable bool
+var name, script string
+var order int
+var enabled bool
 
 func rulesCmd(cli *cli) *cobra.Command {
 	cmd := &cobra.Command{
@@ -41,29 +43,39 @@ func listRulesCmd(cli *cli) *cobra.Command {
 }
 
 func createRulesCmd(cli *cli) *cobra.Command {
+	var flags struct {
+		name    string
+		script  string
+		order   int
+		enabled bool
+	}
 	cmd := &cobra.Command{
-		Use:              "create [OPTIONS]",
-		TraverseChildren: true,
-		Short:            "Create a new rule",
-		Long:             "Create a new rule in your current tenant.",
-		Args:             cobra.MinimumNArgs(2),
+		Use:   "create",
+		Short: "Create a new rule",
+		Long:  "Create a new rule in your current tenant.",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			// TODO: Add the proper functionality here -- currently not working
-			err := cli.api.Client.Rule.Create()
+			r := &management.Rule{
+				Name:    &flags.name,
+				Script:  &flags.script,
+				Order:   &flags.order,
+				Enabled: &flags.enabled,
+			}
+
+			err := cli.api.Client.Rule.Create(r)
 
 			if err != nil {
 				return err
 			}
 
-			cli.renderer.Infof("Your rule `%s` was successfully created.", args[0])
+			cli.renderer.Infof("Your rule `%s` was successfully created.", flags.name)
 			return nil
 		},
 	}
 
 	cmd.Flags().StringVarP(&name, "name", "n", "", "Name of this rule (required)")
 	cmd.Flags().StringVarP(&script, "script", "s", "", "Code to be executed when this rule runs (required)")
-	cmd.Flags().StringVarP(&order, "order", "o", "", "Order that this rule should execute in relative to other rules. Lower-valued rules execute first.")
-	cmd.Flags().BoolVarP(&enable, "enable", "e", false, "Whether the rule is enabled (true), or disabled (false).")
+	cmd.Flags().IntVarP(&order, "order", "o", 0, "Order that this rule should execute in relative to other rules. Lower-valued rules execute first.")
+	cmd.Flags().BoolVarP(&enabled, "enabled", "e", false, "Whether the rule is enabled (true), or disabled (false).")
 	cmd.MarkFlagRequired("name")
 	cmd.MarkFlagRequired("script")
 
