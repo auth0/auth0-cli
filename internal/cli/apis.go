4a12c4cba791a02dc8a3b68da91f06727b0c371f
Rita Zerrizuela <zeta@widcket.com>
2021-01-25 20:13:28 -0300

    feat: apis update

--- internal/cli/apis.go
+++ internal/cli/apis.go
@@ -15,6 +15,7 @@ func apisCmd(cli *cli) *cobra.Command {
 	cmd.SetUsageTemplate(resourceUsageTemplate())
 	cmd.AddCommand(listApisCmd(cli))
 	cmd.AddCommand(createApiCmd(cli))
+	cmd.AddCommand(updateApiCmd(cli))
 
 	return cmd
 }
@@ -42,7 +43,7 @@ Lists your existing APIs. To create one try:
 				return err
 			}
 
-			cli.renderer.ApisList(list.ResourceServers)
+			cli.renderer.ApiList(list.ResourceServers)
 			return nil
 		},
 	}
@@ -86,6 +87,44 @@ auth0 apis create --name myapi --identifier http://my-api
 	cmd.Flags().StringVarP(&flags.identifier, "identifier", "i", "", "Identifier of the API.")
 
 	mustRequireFlags(cmd, "name", "identifier")
+
+	return cmd
+}
+
+func updateApiCmd(cli *cli) *cobra.Command {
+	var flags struct {
+		id         string
+		name       string
+	}
+
+	cmd := &cobra.Command{
+		Use:   "update",
+		Short: "Update an API",
+		Long: `Updates an API:
+
+auth0 apis update --id id --name myapi
+`,
+		RunE: func(cmd *cobra.Command, args []string) error {
+			api := &management.ResourceServer{Name: &flags.name}
+			manager := &management.ResourceServerManager{Management: cli.api}
+
+			err := ansi.Spinner("Updating API", func() error {
+				return manager.Update(flags.id, api)
+			})
+
+			if err != nil {
+				return err
+			}
+
+			cli.renderer.ApiUpdate(api)
+			return nil
+		},
+	}
+
+	cmd.Flags().StringVarP(&flags.id, "id", "i", "", "ID of the API.")
+	cmd.Flags().StringVarP(&flags.name, "name", "n", "", "Name of the API.")
+
+	mustRequireFlags(cmd, "id", "name")
 
 	return cmd
 }
