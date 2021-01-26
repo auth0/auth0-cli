9c869a041e40d47ff22167e24c3e7e2230b9c41f
Danny Turcotte <danny.turcotte@auth0.com>
2021-01-26 16:46:43 -0500

    vendora

--- vendor/gopkg.in/auth0.v5/management/actions.go
+++ vendor/gopkg.in/auth0.v5/management/actions.go
@@ -152,8 +152,7 @@ func (m *ActionVersionManager) Create(actionID string, v *ActionVersion) error {
 	return m.Request("POST", m.URI("actions", "actions", actionID, "versions"), v)
 }
 
-// TODO(cyx): This isn't implemented yet.
-func (m *ActionVersionManager) Update(actionID string, v *ActionVersion) error {
+func (m *ActionVersionManager) UpsertDraft(actionID string, v *ActionVersion) error {
 	return m.Request("PATCH", m.URI("actions", "actions", actionID, "versions", "draft"), v)
 }
 
@@ -176,7 +175,7 @@ func (m *ActionVersionManager) List(actionID string, opts ...RequestOption) (c *
 // name.
 func (m *ActionVersionManager) Promote(actionID, id string) (*ActionVersion, error) {
 	var v ActionVersion
-	err := m.Request("POST", m.URI("actions", "actions", actionID, "versions", id, "promote"), &v)
+	err := m.Request("POST", m.URI("actions", "actions", actionID, "versions", id, "deploy"), &v)
 	return &v, err
 }
 
