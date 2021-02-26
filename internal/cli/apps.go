212ac9549e99b82896eb9c83c97b599653432817
Rita Zerrizuela <zeta@widcket.com>
2021-02-26 20:54:45 -0300

    Replace ID flags with arguments

--- internal/cli/apps.go
+++ internal/cli/apps.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"errors"
 	"fmt"
 	"strings"
 
@@ -27,6 +28,7 @@ func appsCmd(cli *cli) *cobra.Command {
 
 	cmd.SetUsageTemplate(resourceUsageTemplate())
 	cmd.AddCommand(listAppsCmd(cli))
+	cmd.AddCommand(showAppCmd(cli))
 	cmd.AddCommand(createAppCmd(cli))
 	cmd.AddCommand(updateAppCmd(cli))
 	cmd.AddCommand(deleteAppCmd(cli))
@@ -63,28 +65,87 @@ Lists your existing applications. To create one try:
 	return cmd
 }
 
-func deleteAppCmd(cli *cli) *cobra.Command {
-	var flags struct {
+func showAppCmd(cli *cli) *cobra.Command {
+	var inputs struct {
 		ID string
 	}
 
 	cmd := &cobra.Command{
-		Use:   "delete",
-		Short: "Delete an application",
-		Long: `Delete an application:
+		Use:   "show",
+		Args:  cobra.MaximumNArgs(1),
+		Short: "Show an application",
+		Long: `Show an application:
 
-auth0 apps delete --id id
+auth0 apps show <id>
 `,
 		PreRun: func(cmd *cobra.Command, args []string) {
 			prepareInteractivity(cmd)
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if shouldPrompt(cmd, appID) {
-				input := prompt.TextInput(appID, "Id:", "Id of the application.", true)
+			if len(args) == 0 {
+				if shouldPrompt(cmd, appID) {
+					input := prompt.TextInput(appID, "Id:", "Id of the application.", true)
 
-				if err := prompt.AskOne(input, &flags); err != nil {
-					return err
+					if err := prompt.AskOne(input, &inputs); err != nil {
+						return err
+					}
+				} else {
+					return errors.New("missing application id")
 				}
+			} else {
+				inputs.ID = args[0]
+			}
+
+			a := &management.Client{ClientID: &inputs.ID}
+
+			err := ansi.Spinner("Loading application", func() error {
+				var err error
+				a, err = cli.api.Client.Read(inputs.ID)
+				return err
+			})
+
+			if err != nil {
+				return err
+			}
+
+			revealClientSecret := auth0.StringValue(a.AppType) != "native" && auth0.StringValue(a.AppType) != "spa"
+			cli.renderer.ApplicationShow(a, revealClientSecret)
+			return nil
+		},
+	}
+
+	return cmd
+}
+
+func deleteAppCmd(cli *cli) *cobra.Command {
+	var inputs struct {
+		ID string
+	}
+
+	cmd := &cobra.Command{
+		Use:   "delete",
+		Args:  cobra.MaximumNArgs(1),
+		Short: "Delete an application",
+		Long: `Delete an application:
+
+auth0 apps delete <id>
+`,
+		PreRun: func(cmd *cobra.Command, args []string) {
+			prepareInteractivity(cmd)
+		},
+		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) == 0 {
+				if shouldPrompt(cmd, appID) {
+					input := prompt.TextInput(appID, "Id:", "Id of the application.", true)
+
+					if err := prompt.AskOne(input, &inputs); err != nil {
+						return err
+					}
+				} else {
+					return errors.New("missing application id")
+				}
+			} else {
+				inputs.ID = args[0]
 			}
 
 			if !cli.force && canPrompt(cmd) {
@@ -94,13 +155,10 @@ auth0 apps delete --id id
 			}
 
 			return ansi.Spinner("Deleting application", func() error {
-				return cli.api.Client.Delete(flags.ID)
+				return cli.api.Client.Delete(inputs.ID)
 			})
 		},
 	}
-
-	cmd.Flags().StringVarP(&flags.ID, appID, "i", "", "ID of the application.")
-	mustRequireFlags(cmd, appID)
 
 	return cmd
 }
@@ -199,7 +257,7 @@ auth0 apps create --name myapp --type [native|spa|regular|m2m]
 }
 
 func updateAppCmd(cli *cli) *cobra.Command {
-	var flags struct {
+	var inputs struct {
 		ID          string
 		Name        string
 		Type        string
@@ -210,27 +268,34 @@ func updateAppCmd(cli *cli) *cobra.Command {
 
 	cmd := &cobra.Command{
 		Use:   "update",
-		Short: "Update a new application",
-		Long: `Update a new application:
+		Args:  cobra.MaximumNArgs(1),
+		Short: "Update an application",
+		Long: `Update an application:
 
-auth0 apps update --id id --name myapp --type [native|spa|regular|m2m]
+auth0 apps update <id> --name myapp --type [native|spa|regular|m2m]
 `,
 		PreRun: func(cmd *cobra.Command, args []string) {
 			prepareInteractivity(cmd)
 		},
 		RunE: func(cmd *cobra.Command, args []string) error {
-			if shouldPrompt(cmd, appID) {
-				input := prompt.TextInput(appID, "Id:", "Id of the application.", true)
+			if len(args) == 0 {
+				if shouldPrompt(cmd, appID) {
+					input := prompt.TextInput(appID, "Id:", "Id of the application.", true)
 
-				if err := prompt.AskOne(input, &flags); err != nil {
-					return err
+					if err := prompt.AskOne(input, &inputs); err != nil {
+						return err
+					}
+				} else {
+					return errors.New("missing application id")
 				}
+			} else {
+				inputs.ID = args[0]
 			}
 
 			if shouldPrompt(cmd, appName) {
 				input := prompt.TextInput(appName, "Name:", "Name of the application", true)
 
-				if err := prompt.AskOne(input, &flags); err != nil {
+				if err := prompt.AskOne(input, &inputs); err != nil {
 					return err
 				}
 			}
@@ -246,7 +311,7 @@ auth0 apps update --id id --name myapp --type [native|spa|regular|m2m]
 					[]string{"Native", "Single Page Web Application", "Regular Web Application", "Machine to Machine"},
 					true)
 
-				if err := prompt.AskOne(input, &flags); err != nil {
+				if err := prompt.AskOne(input, &inputs); err != nil {
 					return err
 				}
 			}
@@ -254,21 +319,21 @@ auth0 apps update --id id --name myapp --type [native|spa|regular|m2m]
 			if shouldPrompt(cmd, appDescription) {
 				input := prompt.TextInput(appDescription, "Description:", "Description of the application.", false)
 
-				if err := prompt.AskOne(input, &flags); err != nil {
+				if err := prompt.AskOne(input, &inputs); err != nil {
 					return err
 				}
 			}
 
 			a := &management.Client{
-				Name:                    &flags.Name,
-				Description:             &flags.Description,
-				AppType:                 auth0.String(apiTypeFor(flags.Type)),
-				Callbacks:               apiCallbacksFor(flags.Callbacks),
-				TokenEndpointAuthMethod: apiAuthMethodFor(flags.AuthMethod),
+				Name:                    &inputs.Name,
+				Description:             &inputs.Description,
+				AppType:                 auth0.String(apiTypeFor(inputs.Type)),
+				Callbacks:               apiCallbacksFor(inputs.Callbacks),
+				TokenEndpointAuthMethod: apiAuthMethodFor(inputs.AuthMethod),
 			}
 
 			err := ansi.Spinner("Updating application", func() error {
-				return cli.api.Client.Update(flags.ID, a)
+				return cli.api.Client.Update(inputs.ID, a)
 			})
 
 			if err != nil {
@@ -283,17 +348,15 @@ auth0 apps update --id id --name myapp --type [native|spa|regular|m2m]
 		},
 	}
 
-	cmd.Flags().StringVarP(&flags.ID, appID, "i", "", "ID of the application.")
-	cmd.Flags().StringVarP(&flags.Name, "name", "n", "", "Name of the application.")
-	cmd.Flags().StringVarP(&flags.Type, "type", "t", "", "Type of application:\n"+
+	cmd.Flags().StringVarP(&inputs.Name, "name", "n", "", "Name of the application.")
+	cmd.Flags().StringVarP(&inputs.Type, "type", "t", "", "Type of application:\n"+
 		"- native: mobile, desktop, CLI and smart device apps running natively.\n"+
 		"- spa (single page application): a JavaScript front-end app that uses an API.\n"+
 		"- regular: Traditional web app using redirects.\n"+
 		"- m2m (machine to machine): CLIs, daemons or services running on your backend.")
-	cmd.Flags().StringVarP(&flags.Description, "description", "d", "", "Description of the application. Max character count is 140.")
-	cmd.Flags().StringSliceVarP(&flags.Callbacks, "callbacks", "c", nil, "After the user authenticates we will only call back to any of these URLs. You can specify multiple valid URLs by comma-separating them (typically to handle different environments like QA or testing). Make sure to specify the protocol (https://) otherwise the callback may fail in some cases. With the exception of custom URI schemes for native apps, all callbacks should use protocol https://.")
-	cmd.Flags().StringVar(&flags.AuthMethod, "auth-method", "", "Defines the requested authentication method for the token endpoint. Possible values are 'None' (public application without a client secret), 'Post' (application uses HTTP POST parameters) or 'Basic' (application uses HTTP Basic).")
-	mustRequireFlags(cmd, appID)
+	cmd.Flags().StringVarP(&inputs.Description, "description", "d", "", "Description of the application. Max character count is 140.")
+	cmd.Flags().StringSliceVarP(&inputs.Callbacks, "callbacks", "c", nil, "After the user authenticates we will only call back to any of these URLs. You can specify multiple valid URLs by comma-separating them (typically to handle different environments like QA or testing). Make sure to specify the protocol (https://) otherwise the callback may fail in some cases. With the exception of custom URI schemes for native apps, all callbacks should use protocol https://.")
+	cmd.Flags().StringVar(&inputs.AuthMethod, "auth-method", "", "Defines the requested authentication method for the token endpoint. Possible values are 'None' (public application without a client secret), 'Post' (application uses HTTP POST parameters) or 'Basic' (application uses HTTP Basic).")
 
 	return cmd
 }
