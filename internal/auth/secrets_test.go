cdb3f3970f486f371fc707d0df978fbaf3bdd1bd
Jorge L. Fatta <jorge.fatta@auth0.com>
2021-03-12 15:55:32 -0300

    test: keychain abstraction (secrets)

---
+++ internal/auth/secrets_test.go
@@ -0,0 +1,60 @@
+package auth
+
+import (
+	"testing"
+
+	"github.com/zalando/go-keyring"
+)
+
+func TestSecrets(t *testing.T) {
+	t.Run("fails: not found", func(t *testing.T) {
+		// init underlying keychain manager
+		keyring.MockInit()
+
+		kr := &Keyring{}
+		_, err := kr.Get("mynamespace", "foo")
+
+		if got, want := err, keyring.ErrNotFound; got != want {
+			t.Fatalf("wanted error: %v, got: %v", want, got)
+		}
+	})
+
+	t.Run("succeeds: get secret", func(t *testing.T) {
+		// init underlying keychain manager
+		keyring.MockInit()
+
+		// set with the underlying manager:
+		err := keyring.Set("mynamespace", "foo", "bar")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		kr := &Keyring{}
+		v, err := kr.Get("mynamespace", "foo")
+
+		if got, want := v, "bar"; got != want {
+			t.Fatalf("wanted error: %v, got: %v", want, got)
+		}
+	})
+
+	t.Run("succeds: set secret", func(t *testing.T) {
+		// init underlying keychain manager
+		keyring.MockInit()
+
+		kr := &Keyring{}
+		err := kr.Set("mynamespace", "foo", "bar")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		// get with the underlying manager:
+		v, err := keyring.Get("mynamespace", "foo")
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		if got, want := v, "bar"; got != want {
+			t.Fatalf("wanted secret: %v, got: %v", want, got)
+		}
+	})
+}
