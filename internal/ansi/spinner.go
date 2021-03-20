7bf7c0b78522400d5b1c7cab22d21d4c3ada5dcc
Cyril David <cyx@users.noreply.github.com>
2021-03-20 09:35:54 -0700

    add more spinner behavior (#173)
    As I use the CLI more, I find the spinner text that gets left behind
    isn't actually useful -- it's only useful while it's happening.
    
    After the message was completed, the user will have two states:
    
    It was successful, at which point the informational text that tells
    them why they're waiting is no longer useful.
    It failed, at which point, we'd also have separate error messages
    below the spinner text.
    In summary, it's good to have the text in transient form, and clear it
    out when done.
    
    == Proposal
    
    1. `Waiting` - used just to indicate that there's activity going on. No
       message necessary since after the wait, the outcome is obvious.
    
    2. `Spinner` - same behavior as before. Useful for scenarios like
       deleting, since there's nothing to show after, and having the
       classic:
    
       `Deleting application... done`
    
       Text is better UX.

--- internal/ansi/spinner.go
+++ internal/ansi/spinner.go
@@ -1,7 +1,6 @@
 package ansi
 
 import (
-	"fmt"
 	"os"
 	"time"
 
@@ -13,18 +12,30 @@ const (
 	spinnerTextDone     = "done"
 	spinnerTextFailed   = "failed"
 
-	spinnerColor = "red"
+	spinnerColor = "blue"
 )
 
+func Waiting(fn func() error) error {
+	return loading("", "", "", fn)
+}
+
 func Spinner(text string, fn func() error) error {
+	initialMsg := text + spinnerTextEllipsis + " "
+	doneMsg := initialMsg + spinnerTextDone
+	failMsg := initialMsg + spinnerTextFailed
+
+	return loading(initialMsg, doneMsg, failMsg, fn)
+}
+
+func loading(initialMsg, doneMsg, failMsg string, fn func() error) error {
 	done := make(chan struct{})
 	errc := make(chan error)
 	go func() {
 		defer close(done)
 
 		s := spinner.New(spinner.CharSets[11], 100*time.Millisecond, spinner.WithWriter(os.Stderr))
-		s.Prefix = text + spinnerTextEllipsis + " "
-		s.FinalMSG = s.Prefix + spinnerTextDone
+		s.Prefix = initialMsg
+		s.FinalMSG = doneMsg
 
 		if err := s.Color(spinnerColor); err != nil {
 			panic(err)
@@ -33,7 +44,7 @@ func Spinner(text string, fn func() error) error {
 		s.Start()
 		err := <-errc
 		if err != nil {
-			s.FinalMSG = s.Prefix + spinnerTextFailed
+			s.FinalMSG = failMsg
 		}
 
 		s.Stop()
@@ -42,6 +53,5 @@ func Spinner(text string, fn func() error) error {
 	err := fn()
 	errc <- err
 	<-done
-	fmt.Println()
 	return err
 }
