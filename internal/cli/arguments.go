23d190ab0eb73b9eac3a33d6f8e73431552f3b72
Cyril David <cyril.david@auth0.com>
2021-03-23 01:28:19 -0700

    Rules: first stab at generalizing picker options
    This is mostly an internal only change which extracts the behavior of
    having `pickers`.
    
    == TODO
    
    - [ ] Try it out with apps
    - [ ] Try it out with apis

--- internal/cli/arguments.go
+++ internal/cli/arguments.go
@@ -3,6 +3,7 @@ package cli
 import (
 	"fmt"
 
+	"github.com/auth0/auth0-cli/internal/ansi"
 	"github.com/spf13/cobra"
 )
 
@@ -29,6 +30,32 @@ func (a Argument) GetIsRequired() bool {
 
 func (a *Argument) Ask(cmd *cobra.Command, value interface{}) error {
 	return askArgument(cmd, a, value)
+}
+
+type pickerOptionsFunc func() (pickerOptions, error)
+
+func (a *Argument) Picker(cmd *cobra.Command, result *string, fn pickerOptionsFunc) error {
+	var opts pickerOptions
+	err := ansi.Waiting(func() error {
+		var err error
+		opts, err = fn()
+		return err
+	})
+
+	if err != nil {
+		return err
+	}
+
+	// TODO(cyx): Fix this up. For now everything depends on `flag`.
+	f := Flag{Name: a.Name}
+	defaultLabel := opts.defaultLabel()
+	var val string
+	if err := selectFlag(cmd, &f, &val, opts.labels(), &defaultLabel, false); err != nil {
+		return err
+	}
+
+	*result = opts.getValue(val)
+	return nil
 }
 
 func askArgument(cmd *cobra.Command, i commandInput, value interface{}) error {
