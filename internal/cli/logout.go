f1c5ab2a259de0e327fb78659f47a2f627eb2abf
Jorge L. Fatta <jorge.fatta@auth0.com>
2021-03-30 10:12:25 -0300

    fix: logout (#212)

--- internal/cli/logout.go
+++ internal/cli/logout.go
@@ -1,6 +1,7 @@
 package cli
 
 import (
+	"errors"
 	"fmt"
 
 	"github.com/auth0/auth0-cli/internal/prompt"
@@ -12,7 +13,7 @@ func logoutCmd(cli *cli) *cobra.Command {
 		Use:   "logout",
 		Short: "Logout of a tenant's session",
 		Long:  `auth0 logout <tenant>`,
-		Args:    cobra.MaximumNArgs(1),
+		Args:  cobra.MaximumNArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
 			// NOTE(cyx): This was mostly copy/pasted from tenants
 			// use command. Consider refactoring.
@@ -23,12 +24,16 @@ func logoutCmd(cli *cli) *cobra.Command {
 					return fmt.Errorf("Unable to load tenants due to an unexpected error: %w", err)
 				}
 
+				if len(tens) == 0 {
+					return errors.New("there are no tenants available to perform the logout")
+				}
+
 				tenNames := make([]string, len(tens))
 				for i, t := range tens {
 					tenNames[i] = t.Name
 				}
 
-				input := prompt.SelectInput("tenant", "Tenant:", "Tenant to activate", tenNames, "", true)
+				input := prompt.SelectInput("tenant", "Tenant:", "Tenant to logout", tenNames, tenNames[0], true)
 				if err := prompt.AskOne(input, &selectedTenant); err != nil {
 					return fmt.Errorf("An unexpected error occurred: %w", err)
 				}
