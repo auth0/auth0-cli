799afb5357fa13e239dbc430df606a7c810a40b4
Rita Zerrizuela <zeta@widcket.com>
2021-04-14 18:39:03 -0300

    Give more space to timestamp

--- internal/display/logs.go
+++ internal/display/logs.go
@@ -77,7 +77,7 @@ func (v *logView) AsTableRow() []string {
 	return []string{
 		typ,
 		truncate(desc, 50),
-		ansi.Faint(truncate(timeAgo(v.GetDate()), 14)),
+		ansi.Faint(truncate(timeAgo(v.GetDate()), 19)),
 		conn,
 		clientName,
 	}
@@ -122,7 +122,7 @@ func (v *logView) typeDesc() (typ, desc string) {
 		typ = "..."
 	}
 
-	typ = truncate(chunks[0], 25)
+	typ = truncate(chunks[0], 20)
 
 	if len(chunks) == 2 {
 		desc = strings.TrimSuffix(chunks[1], ")")
