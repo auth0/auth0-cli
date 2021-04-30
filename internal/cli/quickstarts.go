88c52f80d50c6288036703bccefe8e011fac763d
Cyril David <cyril.david@auth0.com>
2021-04-30 15:11:38 -0700

    fix: just make client.List / Read work for all cases
    Thish should be fixed on the SDK level

--- internal/cli/quickstarts.go
+++ internal/cli/quickstarts.go
@@ -106,10 +106,10 @@ func downloadQuickstartCmd(cli *cli) *cobra.Command {
 	}
 
 	cmd := &cobra.Command{
-		Use:     "download",
-		Args:    cobra.MaximumNArgs(1),
-		Short:   "Download a Quickstart sample app for a specific tech stack",
-		Long:    "Download a Quickstart sample app for a specific tech stack.",
+		Use:   "download",
+		Args:  cobra.MaximumNArgs(1),
+		Short: "Download a Quickstart sample app for a specific tech stack",
+		Long:  "Download a Quickstart sample app for a specific tech stack.",
 		Example: `auth0 quickstarts download --stack <stack>
 auth0 qs download --stack <stack>`,
 		PreRun: func(cmd *cobra.Command, args []string) {
@@ -132,7 +132,7 @@ auth0 qs download --stack <stack>`,
 			var client *management.Client
 			err := ansi.Waiting(func() error {
 				var err error
-				client, err = cli.api.Client.Read(inputs.ClientID)
+				client, err = cli.api.Client.Read(inputs.ClientID, management.ExcludeFields(clientExcludedList...))
 				return err
 			})
 
