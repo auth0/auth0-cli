f182da99c04aba7ac50e768a88f76bc7917c2dad
morganelle <morgan.nomura@auth0.com>
2021-04-30 17:13:38 -0700

    update support scripts since escape characters are vremoved

--- integration/get-client-id.sh
+++ integration/get-client-id.sh
@@ -1,6 +1,5 @@
 #! /bin/bash
 
-output=$( auth0 apps create -n newapp -t native --description newapp --format json --no-input )
-app=${output:12}
+app=$( auth0 apps create -n newapp -t native --description NewApp --format json --no-input )
 
-echo $app | jq -r '.["client_id"]' > ./integration/apps/client-id
+echo "$app" | jq -r '.["client_id"]' > ./integration/client-id
--- integration/test-cleanup.sh
+++ integration/test-cleanup.sh
@@ -1,20 +1,18 @@
 #! /bin/bash
 
-# update when escape character bug fixed
-output=$( auth0 apps list --format json --no-input)
-apps=${output:12}
+apps=$( auth0 apps list --format json --no-input)
 
 for app in $( echo "${apps}" | jq -r '.[] | @base64' ); do
     _jq() {
-     echo ${app} | base64 --decode | jq -r ${1}
+     echo "${app}" | base64 --decode | jq -r "${1}"
     }
 
-    clientid=$( echo $(_jq '.ClientID') )
-    name=$( echo $(_jq '.Name') )
+    clientid=$(_jq '.ClientID')
+    name=$(_jq '.Name')
 
     if [[ $name != "Default App" ]]
     then
-        echo deleting $name
-        echo $( auth0 apps delete $clientid)
+        echo deleting "$name"
+        $( auth0 apps delete "$clientid")
     fi
 done
