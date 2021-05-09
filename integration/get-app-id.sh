5ef2044727d3e9644d2e85122d52accf9470bafc
Rita Zerrizuela <zeta@widcket.com>
2021-05-09 09:27:37 -0300

    Add mkdir -p

--- integration/get-app-id.sh
+++ integration/get-app-id.sh
@@ -2,4 +2,5 @@
 
 app=$( auth0 apps create -n integration-test-app-newapp -t native --description NewApp --format json --no-input )
 
+mkdir -p ./integration/identifiers
 echo "$app" | jq -r '.["client_id"]' > ./integration/identifiers/app-id
