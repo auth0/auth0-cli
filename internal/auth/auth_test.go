5b0fe535d7630ff236d9ccdeba078399ff78e01e
Cyril David <cyril.david@auth0.com>
2021-05-14 15:24:31 -0700

    Update scopes to add roles; update tests

--- internal/auth/auth_test.go
+++ internal/auth/auth_test.go
@@ -7,7 +7,9 @@ func TestRequiredScopes(t *testing.T) {
 		crudResources := []string{
 			"clients",
 			"resource_servers",
+			"roles",
 			"rules",
+			"users",
 		}
 		crudPrefixes := []string{"create:", "delete:", "read:", "update:"}
 
@@ -24,9 +26,9 @@ func TestRequiredScopes(t *testing.T) {
 
 	t.Run("verify special scopes", func(t *testing.T) {
 		list := []string{
-			"read:client_keys", "read:logs",
-			"read:users", "update:users",
 			"read:branding", "update:branding",
+			"read:connections", "update:connections",
+			"read:custom_domains",
 			"read:client_keys", "read:logs", "read:tenant_settings",
 		}
 
