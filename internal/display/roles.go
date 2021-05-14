20dad88d8175651ff73341b7fc3c25816c814837
Cyril David <cyril.david@auth0.com>
2021-05-14 16:00:36 -0700

    Complete rest of CRUD commands

--- internal/display/roles.go
+++ internal/display/roles.go
@@ -2,7 +2,6 @@ package display
 
 import (
 	"github.com/auth0/auth0-cli/internal/ansi"
-	"github.com/auth0/auth0-cli/internal/auth0"
 	"gopkg.in/auth0.v5/management"
 )
 
@@ -21,6 +20,14 @@ func (v *roleView) AsTableRow() []string {
 		v.Name,
 		ansi.Faint(v.ID),
 		v.Description,
+	}
+}
+
+func (v *roleView) KeyValues() [][]string {
+	return [][]string{
+		[]string{"ID", ansi.Faint(v.ID)},
+		[]string{"NAME", v.Name},
+		[]string{"DESCRIPTION", v.Description},
 	}
 }
 
@@ -47,29 +54,25 @@ func (r *Renderer) RoleList(roles []*management.Role) {
 	r.Results(res)
 }
 
-func (r *Renderer) RoleGet(role *management.Role) {
-	r.Heading(ansi.Bold(r.Tenant), "role\n")
-	r.Results([]View{&roleView{
-		Name:        auth0.StringValue(role.Name),
-		ID:          auth0.StringValue(role.ID),
-		Description: auth0.StringValue(role.Description),
-	}})
-}
-
-func (r *Renderer) RoleUpdate(role *management.Role) {
-	r.Heading(ansi.Bold(r.Tenant), "role\n")
-	r.Results([]View{&roleView{
-		Name:        auth0.StringValue(role.Name),
-		ID:          auth0.StringValue(role.ID),
-		Description: auth0.StringValue(role.Description),
-	}})
+func (r *Renderer) RoleShow(role *management.Role) {
+	r.Heading("role")
+	r.roleResult(role)
 }
 
 func (r *Renderer) RoleCreate(role *management.Role) {
-	r.Heading(ansi.Bold(r.Tenant), "role\n")
-	r.Results([]View{&roleView{
-		Name:        auth0.StringValue(role.Name),
-		ID:          auth0.StringValue(role.ID),
-		Description: auth0.StringValue(role.Description),
-	}})
+	r.Heading("role created")
+	r.roleResult(role)
+}
+
+func (r *Renderer) RoleUpdate(role *management.Role) {
+	r.Heading("role updated")
+	r.roleResult(role)
+}
+
+func (r *Renderer) roleResult(role *management.Role) {
+	r.Result(&roleView{
+		Name:        role.GetName(),
+		ID:          ansi.Faint(role.GetID()),
+		Description: role.GetDescription(),
+	})
 }
