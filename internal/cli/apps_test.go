67a94e684204b241093a40cfe1aaf83cbe0eabe2
Jorge L. Fatta <jorge.fatta@auth0.com>
2021-05-21 19:02:20 -0300

    feat: apps `--reveal` flag to expose secrets (#301)
    * feat: apps --reveal flag to expose secrets
    
    * hide client secret from ls
    
    * fix: signing keys only available if --reveal
    
    * remove redundant type from array (simplifycompositelit)
    
    * Update internal/cli/apps.go
    
    Co-authored-by: Cyril David <cyx@users.noreply.github.com>
    
    * Update internal/cli/apps.go
    
    Co-authored-by: Rita Zerrizuela <zeta@widcket.com>
    
    Co-authored-by: Cyril David <cyx@users.noreply.github.com>
    Co-authored-by: Rita Zerrizuela <zeta@widcket.com>

--- internal/cli/apps_test.go
+++ internal/cli/apps_test.go
@@ -12,46 +12,77 @@ import (
 )
 
 func TestAppsListCmd(t *testing.T) {
-	// Step 1: Setup our client mock for this test. We only care about
-	// Clients so no need to bootstrap other bits.
-	ctrl := gomock.NewController(t)
-	defer ctrl.Finish()
-
-	clientAPI := auth0.NewMockClientAPI(ctrl)
-	clientAPI.EXPECT().
-		List(gomock.Any()).
-		Return(&management.ClientList{
-			Clients: []*management.Client{
-				{
-					Name:      auth0.String("some-name"),
-					ClientID:  auth0.String("some-id"),
-					Callbacks: stringToInterfaceSlice([]string{"http://localhost"}),
-				},
+	tests := []struct {
+		name         string
+		assertOutput func(t testing.TB, out string)
+		args         []string
+	}{
+		{
+			name: "happy path",
+			assertOutput: func(t testing.TB, out string) {
+				expectTable(t, out,
+					[]string{"CLIENT ID", "NAME", "TYPE"},
+					[][]string{
+						{"some-id", "some-name", "Generic"},
+					},
+				)
 			},
-		}, nil)
-
-	stdout := &bytes.Buffer{}
-
-	// Step 2: Setup our cli context. The important bits are
-	// renderer and api.
-	cli := &cli{
-		renderer: &display.Renderer{
-			MessageWriter: ioutil.Discard,
-			ResultWriter:  stdout,
 		},
-		api: &auth0.API{Client: clientAPI},
+		{
+			name: "reveal secrets",
+			args: []string{"--reveal"},
+			assertOutput: func(t testing.TB, out string) {
+				expectTable(t, out,
+					[]string{"CLIENT ID", "NAME", "TYPE", "CLIENT SECRET"},
+					[][]string{
+						{"some-id", "some-name", "Generic", "secret-here"},
+					},
+				)
+			},
+		},
 	}
 
-	cmd := listAppsCmd(cli)
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			// Step 1: Setup our client mock for this test. We only care about
+			// Clients so no need to bootstrap other bits.
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
 
-	if err := cmd.Execute(); err != nil {
-		t.Fatal(err)
+			clientAPI := auth0.NewMockClientAPI(ctrl)
+			clientAPI.EXPECT().
+				List(gomock.Any()).
+				Return(&management.ClientList{
+					Clients: []*management.Client{
+						{
+							Name:         auth0.String("some-name"),
+							ClientID:     auth0.String("some-id"),
+							Callbacks:    stringToInterfaceSlice([]string{"http://localhost"}),
+							ClientSecret: auth0.String("secret-here"),
+						},
+					},
+				}, nil)
+
+			stdout := &bytes.Buffer{}
+
+			// Step 2: Setup our cli context. The important bits are
+			// renderer and api.
+			cli := &cli{
+				renderer: &display.Renderer{
+					MessageWriter: ioutil.Discard,
+					ResultWriter:  stdout,
+				},
+				api: &auth0.API{Client: clientAPI},
+			}
+
+			cmd := listAppsCmd(cli)
+			cmd.SetArgs(test.args)
+
+			if err := cmd.Execute(); err != nil {
+				t.Fatal(err)
+			}
+
+			test.assertOutput(t, stdout.String())
+		})
 	}
-
-	expectTable(t, stdout.String(),
-		[]string{"CLIENT ID", "NAME", "TYPE"},
-		[][]string{
-			{"some-id", "some-name", "Generic"},
-		},
-	)
 }
