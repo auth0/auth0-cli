ba2e9c2ebe0048a98ac530da1aecfa56f97b627f
bright-poku <bright.adupoku@auth0.com>
2021-05-21 12:44:01 -0400

    [CLI-164] updated scopes

--- pkg/auth0-cli-config-generator/main.go
+++ pkg/auth0-cli-config-generator/main.go
@@ -28,15 +28,17 @@ type params struct {
 }
 
 var requiredScopes = []string{
-	//	"openid",
-	//	"offline_access", // <-- to get a refresh token.
+	"openid",
+	"offline_access", // <-- to get a refresh token.
 	"create:clients", "delete:clients", "read:clients", "update:clients",
 	"create:resource_servers", "delete:resource_servers", "read:resource_servers", "update:resource_servers",
+	"create:roles", "delete:roles", "read:roles", "update:roles",
 	"create:rules", "delete:rules", "read:rules", "update:rules",
-	"read:client_keys", "read:logs", "read:connections", "update:connections",
 	"create:users", "delete:users", "read:users", "update:users",
 	"read:branding", "update:branding",
+	"read:connections", "update:connections",
 	"read:client_keys", "read:logs", "read:tenant_settings", "read:custom_domains",
+	"read:anomaly_blocks", "delete:anomaly_blocks",
 }
 
 func (p params) validate() error {
