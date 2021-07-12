27afbb7fc56810c29724c73428b8ec666a30b4f9
Cyril David <cyx@users.noreply.github.com>
2021-07-12 14:30:10 -0700

    feat: add role permissions (#329)
    * feat: add roles permissions list/associate/remove
    
    This feature adds a set of commands:
    
    ```
    auth0 roles permissions list
    auth0 roles permissions associate
    auth0 roles permissions remove
    ```
    
    * add/associate
    
    * Better UX
    
    * Lint
    
    * Update internal/display/role_permissions.go
    
    Co-authored-by: Rita Zerrizuela <zeta@widcket.com>
    
    * s/assoc/add
    
    Co-authored-by: Rita Zerrizuela <zeta@widcket.com>

--- internal/auth0/role.go
+++ internal/auth0/role.go
@@ -17,4 +17,19 @@ type RoleAPI interface {
 
 	// Delete a role.
 	Delete(id string, opts ...management.RequestOption) (err error)
+
+	// AssociatePermissions associates permissions to a role.
+	//
+	// See: https://auth0.com/docs/api/management/v2#!/Roles/post_role_permission_assignment
+	AssociatePermissions(id string, permissions []*management.Permission, opts ...management.RequestOption) error
+
+	// Permissions retrieves all permissions granted by a role.
+	//
+	// See: https://auth0.com/docs/api/management/v2#!/Roles/get_role_permission
+	Permissions(id string, opts ...management.RequestOption) (p *management.PermissionList, err error)
+
+	// RemovePermissions removes permissions associated to a role.
+	//
+	// See: https://auth0.com/docs/api/management/v2#!/Roles/delete_role_permission_assignment
+	RemovePermissions(id string, permissions []*management.Permission, opts ...management.RequestOption) error
 }
---
+++ internal/display/role_permissions.go
@@ -0,0 +1,79 @@
+package display
+
+import (
+	"strings"
+
+	"github.com/auth0/auth0-cli/internal/ansi"
+	"gopkg.in/auth0.v5/management"
+)
+
+type rolePermissionView struct {
+	APIID       string
+	APIName     string
+	Name        string
+	Description string
+	raw         interface{}
+}
+
+func (v *rolePermissionView) Object() interface{} {
+	return v.raw
+}
+
+func (v *rolePermissionView) AsTableHeader() []string {
+	return []string{"API Identifier", "API Name", "Permission Name", "Description"}
+}
+
+func (v *rolePermissionView) AsTableRow() []string {
+	return []string{
+		ansi.Faint(v.APIID),
+		v.APIName,
+		v.Name,
+		v.Description,
+	}
+}
+
+func (v *rolePermissionView) KeyValues() [][]string {
+	return [][]string{
+		{"ID", ansi.Faint(v.APIID)},
+		{"NAME", v.APIName},
+		{"PERMISSION NAME", v.Name},
+		{"DESCRIPTION", v.Description},
+	}
+}
+
+func (r *Renderer) RolePermissionList(perms []*management.Permission) {
+	resource := "role permissions"
+
+	r.Heading(resource)
+
+	if len(perms) == 0 {
+		r.EmptyState(resource)
+		r.Infof("Use 'auth0 roles permissions add' to add one")
+		return
+	}
+
+	var res []View
+	for _, perm := range perms {
+		res = append(res, &rolePermissionView{
+			APIName:     perm.GetResourceServerName(),
+			APIID:       perm.GetResourceServerIdentifier(),
+			Name:        perm.GetName(),
+			Description: perm.GetDescription(),
+			raw:         perm,
+		})
+	}
+
+	r.Results(res)
+}
+
+func (r *Renderer) RolePermissionAdd(role *management.Role, rs *management.ResourceServer, perms []string) {
+	r.Heading("role permissions added")
+
+	r.Infof("Added permissions %s (%s) to role %s.", ansi.Green(strings.Join(perms, ", ")), ansi.Faint(rs.GetIdentifier()), ansi.Green(role.GetName()))
+}
+
+func (r *Renderer) RolePermissionRemove(role *management.Role, rs *management.ResourceServer, perms []string) {
+	r.Heading("role permissions removed")
+
+	r.Infof("Removed permissions %s (%s) from role %s.", ansi.Green(strings.Join(perms, ", ")), ansi.Faint(rs.GetIdentifier()), ansi.Green(role.GetName()))
+}
