ed6cba5b5cbc3a2dd79f42a0788aea07db9b3029
Andres Aguiar <andres.aguiar@auth0.com>
2021-07-12 13:03:55 -0300

    Fixed branding documentation

--- internal/cli/branding.go
+++ internal/cli/branding.go
@@ -92,22 +92,22 @@ func templateCmd(cli *cli) *cobra.Command {
 	cmd := &cobra.Command{
 		Use:   "templates",
 		Short: "Manage custom page templates",
-		Long:  `Manage custom [page templates](https://auth0.com/docs/universal-login/new-experience/universal-login-page-templates). This requires a custom domain to be configured for the tenant.
+		Long:  "Manage custom [page templates](https://auth0.com/docs/universal-login/new-experience/universal-login-page-templates). This requires a custom domain to be configured for the tenant.
 
-		This command will open two windows:
-		
-		* A browser window with a [Storybook](https://storybook.js.org/) that shows the login page with the page template applied:
-		
-		![storybook](images/templates-storybook.png)
-		
-		* The default terminal editor, with the page template code:
-		
-		![storybook](images/templates-vs-code.png)
-		
-		You now change the page template code, and the changes will be reflected in the browser window. 
-		
-		Once you close the window, you’ll be asked if you want to save the template. If you answer Yes, the template will be uploaded to your tenant.
-		`,
+This command will open two windows:
+
+* A browser window with a [Storybook](https://storybook.js.org/) that shows the login page with the page template applied:
+
+![storybook](images/templates-storybook.png)
+
+* The default terminal editor, with the page template code:
+
+![storybook](images/templates-vs-code.png)
+
+You now change the page template code, and the changes will be reflected in the browser window. 
+
+Once you close the window, you’ll be asked if you want to save the template. If you answer Yes, the template will be uploaded to your tenant.
+",
 	}
 
 	cmd.SetUsageTemplate(resourceUsageTemplate())
