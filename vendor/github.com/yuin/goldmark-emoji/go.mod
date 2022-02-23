66721708657490e36e9e3b87e419d523ce0cc28b
Sergiu Ghitea <28300158+sergiughf@users.noreply.github.com>
2022-02-23 16:18:19 +0100

    Remove vendor folder (#396)
    * Remove vendor folder
    
    * Run go mod tidy

--- vendor/github.com/yuin/goldmark-emoji/go.mod
+++
@@ -1,5 +0,0 @@
-module github.com/yuin/goldmark-emoji
-
-go 1.15
-
-require github.com/yuin/goldmark v1.2.1
--- vendor/gopkg.in/auth0.v5/meta.go
+++
@@ -1,5 +0,0 @@
-package auth0
-
-// Version of this library. This value is generated automatically during the
-// release process; DO NOT EDIT.
-var Version = "latest"
