8b7a0a5e7c090e4374198241afa0612294749fba
Evan Sims <hello@evansims.com>
2022-03-09 15:14:36 -0600

    Implement JSON config reader

--- internal/cli/import/config.go
+++ internal/cli/import/config.go
@@ -1,8 +1,29 @@
 package cli
 
-// Put here the logic for parsing the JSON config file
+import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
+)
 
 type Config struct {
 	Auth0Domain                 string   `json:"AUTH0_DOMAIN"`
 	Auth0KeywordReplaceMappings struct{} `json:"AUTH0_KEYWORD_REPLACE_MAPPINGS"`
+}
+
+func getConfig(path string) *Config {
+	file, error := ioutil.ReadFile(path)
+
+	if error != nil {
+		log.Printf("Error reading config: #%v ", error)
+	}
+
+	c := &Config{}
+	error = json.Unmarshal([]byte(file), &c)
+
+	if error != nil {
+		log.Printf("Error processing config structure: #%v ", error)
+	}
+
+	return c
 }
