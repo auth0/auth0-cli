80ced390cdab070ee44300e25ccb990f90f7b7c1
Sergiu Ghitea <sergiu.ghitea@okta.com>
2022-11-18 21:16:18 +0100

    Reorganize integration test files into test folder

--- integration/get-rule-id.sh
+++
@@ -1,8 +0,0 @@
-#! /bin/bash
-
-json='{"name":"integration-test-rule-newRule","script":"function(user, context, cb) {\n  cb(null, user, context);\n}\n","enabled":false}'
-rule=$( echo "$json" | auth0 rules create --format json )
-
-mkdir -p ./integration/identifiers
-echo "$rule" | jq -r '.["id"]' > ./integration/identifiers/rule-id
-echo "$rule" | jq '.name = "integration-test-rule-betterName"' | jq '.enabled = false' > ./integration/fixtures/update-rule.json
--- test/integration/scripts/get-api-id.sh
+++ test/integration/scripts/get-api-id.sh
@@ -2,5 +2,5 @@
 
 api=$( auth0 apis create --name integration-test-api-newapi --identifier http://integration-test-api-newapi --scopes read:todos --format json --no-input )
 
-mkdir -p ./integration/identifiers
-echo "$api" | jq -r '.["id"]' > ./integration/identifiers/api-id
+mkdir -p ./test/integration/identifiers
+echo "$api" | jq -r '.["id"]' > ./test/integration/identifiers/api-id
