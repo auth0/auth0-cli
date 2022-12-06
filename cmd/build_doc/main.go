7dc0ac0c4ee9a950e57c9b7b40ae710becc922bc
Will Vedder <willvedd@gmail.com>
2022-12-06 15:29:35 -0500

    DXCDT-284: Remove dead env vars (#545)
    * Removing usage of env vars
    
    * Moving docsPath to closer to where its used
    
    Co-authored-by: Will Vedder <will.vedder@okta.com>

--- cmd/build_doc/main.go
+++ cmd/build_doc/main.go
@@ -1,20 +1,11 @@
 package main
 
 import (
-	"github.com/joeshaw/envdecode"
-
 	"github.com/auth0/auth0-cli/internal/cli"
 )
 
 func main() {
-	var cfg struct {
-		Path string `env:"AUTH0_CLI_DOCS_PATH,default=./docs/"`
-	}
-	if err := envdecode.StrictDecode(&cfg); err != nil {
-		panic(err)
-	}
-
-	err := cli.BuildDoc(cfg.Path)
+	err := cli.BuildDoc()
 	if err != nil {
 		panic(err)
 	}
