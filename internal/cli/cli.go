2bcd603b81ed2b09ffda6df378bcbef090ea3e0d
Will Vedder <willvedd@gmail.com>
2022-12-07 15:37:16 -0500

    Update internal/cli/cli.go
    Co-authored-by: Rita Zerrizuela <zeta@widcket.com>

--- internal/cli/cli.go
+++ internal/cli/cli.go
@@ -221,7 +221,7 @@ func (c *cli) prepareTenant(ctx context.Context) (Tenant, error) {
 			return t, fmt.Errorf("Failed to renew access token. This may occur if the designated application has been deleted or client secret has been rotated. Please re-authenticate by running `auth0 login --as-machine`")
 		}
 
-		c.renderer.Warnf("Failed to renew access token. Please sign in to re-authenticate the CLI.")
+		c.renderer.Warnf("Failed to renew access token. Please log in to re-authorize the CLI.")
 		return RunLoginAsUser(ctx, c)
 
 	}
