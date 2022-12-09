9eec167c00d19e99d8bf16035327c05a5fa11407
Sergiu Ghitea <sergiu.ghitea@okta.com>
2022-12-09 19:10:48 +0100

    Refactor quickstarts command to use quickstart meta URL

--- go.mod
+++ go.mod
@@ -24,7 +24,6 @@ require (
 	github.com/pkg/errors v0.9.1
 	github.com/spf13/cobra v1.6.1
 	github.com/spf13/pflag v1.0.5
-	github.com/spf13/viper v1.14.0
 	github.com/stretchr/testify v1.8.1
 	github.com/tidwall/pretty v1.2.1
 	github.com/zalando/go-keyring v0.2.0
@@ -55,35 +54,27 @@ require (
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/golang/snappy v0.0.4 // indirect
 	github.com/gorilla/css v1.0.0 // indirect
-	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.1 // indirect
 	github.com/klauspost/compress v1.15.11 // indirect
 	github.com/klauspost/pgzip v1.2.5 // indirect
+	github.com/kr/pretty v0.3.0 // indirect
 	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
 	github.com/lestrrat-go/blackmagic v1.0.0 // indirect
 	github.com/lestrrat-go/httpcc v1.0.1 // indirect
 	github.com/lestrrat-go/iter v1.0.1 // indirect
 	github.com/lestrrat-go/option v1.0.0 // indirect
 	github.com/lucasb-eyer/go-colorful v1.2.0 // indirect
-	github.com/magiconair/properties v1.8.6 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-runewidth v0.0.14 // indirect
 	github.com/mgutz/ansi v0.0.0-20200706080929-d51e80ef957d // indirect
 	github.com/microcosm-cc/bluemonday v1.0.21 // indirect
-	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/muesli/reflow v0.3.0 // indirect
 	github.com/muesli/termenv v0.13.0 // indirect
 	github.com/nwaples/rardecode v1.1.0 // indirect
-	github.com/pelletier/go-toml v1.9.5 // indirect
-	github.com/pelletier/go-toml/v2 v2.0.5 // indirect
 	github.com/pierrec/lz4/v4 v4.1.3 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/rivo/uniseg v0.2.0 // indirect
 	github.com/russross/blackfriday/v2 v2.1.0 // indirect
-	github.com/spf13/afero v1.9.2 // indirect
-	github.com/spf13/cast v1.5.0 // indirect
-	github.com/spf13/jwalterweatherman v1.1.0 // indirect
-	github.com/subosito/gotenv v1.4.1 // indirect
 	github.com/ulikunitz/xz v0.5.10 // indirect
 	github.com/xi2/xz v0.0.0-20171230120015-48954b6210f8 // indirect
 	github.com/yuin/goldmark v1.5.2 // indirect
@@ -94,6 +85,6 @@ require (
 	golang.org/x/tools v0.1.12 // indirect
 	google.golang.org/appengine v1.6.7 // indirect
 	google.golang.org/protobuf v1.28.1 // indirect
-	gopkg.in/ini.v1 v1.67.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20180628173108-788fd7840127 // indirect
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
--- internal/auth0/quickstart.go
+++ internal/auth0/quickstart.go
@@ -1,10 +1,150 @@
 package auth0
 
+import (
+	"context"
+	"encoding/json"
+	"fmt"
+	"io"
+	"net/http"
+	"net/url"
+	"os"
+	"path"
+
+	"github.com/auth0/go-auth0/management"
+	"github.com/mholt/archiver/v3"
+)
+
+const (
+	quickstartsMetaURL            = "https://auth0.com/docs/meta/quickstarts"
+	quickstartsOrg                = "auth0-samples"
+	quickstartsDefaultCallbackURL = "https://YOUR_APP/callback"
+)
+
+type Quickstarts []Quickstart
+
 type Quickstart struct {
-	Name    string   `json:"name"`
-	Path    string   `json:"path"`
-	Samples []string `json:"samples"`
-	Org     string   `json:"org"`
-	Repo    string   `json:"repo"`
-	Branch  string   `json:"branch,omitempty"`
+	Name                 string `json:"name"`
+	AppType              string `json:"appType"`
+	URL                  string `json:"url"`
+	Logo                 string `json:"logo"`
+	DownloadLink         string `json:"downloadLink"`
+	DownloadInstructions string `json:"downloadInstructions"`
+}
+
+func (q Quickstart) SamplePath(downloadPath string) (string, error) {
+	query, err := url.ParseQuery(q.DownloadLink)
+	if err != nil {
+		return "", err
+	}
+
+	return path.Join(downloadPath, query.Get("path")), nil
+}
+
+func (q Quickstart) Download(ctx context.Context, downloadPath string, client *management.Client) error {
+	quickstartEndpoint := fmt.Sprintf("https://auth0.com%s", q.DownloadLink)
+	request, err := http.NewRequestWithContext(ctx, http.MethodGet, quickstartEndpoint, nil)
+	if err != nil {
+		return err
+	}
+
+	params := request.URL.Query()
+	params.Add("org", quickstartsOrg)
+	params.Add("client_id", client.GetClientID())
+
+	// Callback URL, if not set, it will just take the default one.
+	callbackURL := quickstartsDefaultCallbackURL
+	if list := client.GetCallbacks(); len(list) > 0 {
+		callbackURL = list[0]
+	}
+	params.Add("callback_url", callbackURL)
+
+	request.URL.RawQuery = params.Encode()
+	request.Header.Set("Content-Type", "application/json")
+
+	response, err := http.DefaultClient.Do(request)
+	if err != nil {
+		return err
+	}
+
+	if response.StatusCode != http.StatusOK {
+		return fmt.Errorf("expected status %d, got %d", http.StatusOK, response.StatusCode)
+	}
+
+	tmpFile, err := os.CreateTemp("", "auth0-quickstart*.zip")
+	if err != nil {
+		return err
+	}
+
+	_, err = io.Copy(tmpFile, response.Body)
+	if err != nil {
+		return err
+	}
+
+	if err := tmpFile.Close(); err != nil {
+		return err
+	}
+	defer os.Remove(tmpFile.Name())
+
+	if err := os.RemoveAll(downloadPath); err != nil {
+		return err
+	}
+
+	return archiver.Unarchive(tmpFile.Name(), downloadPath)
+}
+
+func GetQuickstarts() (Quickstarts, error) {
+	response, err := http.Get(quickstartsMetaURL)
+	if err != nil {
+		return nil, err
+	}
+	defer response.Body.Close()
+
+	if response.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf(
+			"failed to fetch quickstarts metadata, response has status code: %d",
+			response.StatusCode,
+		)
+	}
+
+	var quickstarts Quickstarts
+	if err := json.NewDecoder(response.Body).Decode(&quickstarts); err != nil {
+		return nil, fmt.Errorf("failed to decode quickstarts metadata response: %w", err)
+	}
+
+	return quickstarts, nil
+}
+
+func (q Quickstarts) FindByStack(stack string) (Quickstart, error) {
+	for _, quickstart := range q {
+		if quickstart.Name == stack {
+			return quickstart, nil
+		}
+	}
+
+	return Quickstart{}, fmt.Errorf("quickstart not found for %s", stack)
+}
+
+func (q Quickstarts) FilterByType(qsType string) (Quickstarts, error) {
+	var filteredQuickstarts []Quickstart
+	for _, quickstart := range q {
+		if quickstart.AppType == qsType {
+			filteredQuickstarts = append(filteredQuickstarts, quickstart)
+		}
+	}
+
+	if len(filteredQuickstarts) == 0 {
+		return nil, fmt.Errorf("unable to find any quickstarts for: %s", qsType)
+	}
+
+	return filteredQuickstarts, nil
+}
+
+func (q Quickstarts) Stacks() []string {
+	var stacks []string
+
+	for _, qs := range q {
+		stacks = append(stacks, qs.Name)
+	}
+
+	return stacks
 }
