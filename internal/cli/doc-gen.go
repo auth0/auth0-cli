4f9dd2845061cb14ab596681d3687cf24197460c
Sergiu Ghitea <sergiu.ghitea@okta.com>
2022-12-22 16:26:48 +0100

    Fix link generation and tiny refactor

--- internal/cli/doc-gen.go
+++ internal/cli/doc-gen.go
@@ -13,9 +13,78 @@ import (
 )
 
 const (
-	docsPath                   = "./docs/"
-	associatedCommandsFragment = `{{range .AssociatedCommands}}- [{{.CommandPath}}](auth0_{{.Name}}.md) - {{.Short}}
+	docsPath = "./docs/"
+
+	associatedCommandsFragment = `{{range .AssociatedCommands}}- [{{.CommandPath}}]({{ link .CommandPath }}) - {{.Short}}
 {{end}}`
+
+	parentPageTemplate = `---
+layout: default
+---
+# {{.Name}}
+
+{{.Description}}
+
+## Commands
+
+%s
+`
+
+	homepageTemplate = `---
+layout: home
+---
+
+Build, manage and test your [Auth0](http://auth0.com/) integrations from the command line.
+
+## Installation
+
+Installation instructions available in [project README](https://github.com/auth0/auth0-cli#installation).
+
+## Authenticating to Your Tenant
+
+Authenticating to your Auth0 tenant is required for most functions of the CLI. It can be initiated by running:
+
+{{ wrapWithBackticks "auth0 login" }}
+
+There are two ways to authenticate:
+
+- **As a user** - Recommended when invoking on a personal machine or other interactive environment. Facilitated by [device authorization](https://auth0.com/docs/get-started/authentication-and-authorization-flow/device-authorization-flow) flow.
+- **As a machine** - Recommended when running on a server or non-interactive environments (ex: CI). Facilitated by [client credentials](https://auth0.com/docs/get-started/authentication-and-authorization-flow/client-credentials-flow) flow. Flags available for bypassing interactive shell.
+
+> ⚠️ Authenticating as a user is not supported for **private cloud** tenants. Instead, those users should authenticate with client credentials.
+
+## Available Commands
+
+%s
+`
+
+	commandPageTemplate = `---
+layout: default
+---
+# {{.Name}}
+
+{{.Description}}
+
+{{ wrapWithBackticks .UseLine }}
+
+{{if .HasFlags}}
+## Flags
+
+{{ wrapWithBackticks .Flags }}{{end}}
+
+{{if .HasInheritedFlags}}
+## InheritedFlags
+
+{{ wrapWithBackticks .InheritedFlags }}{{end}}
+
+## Examples
+
+{{ wrapWithBackticks .Examples }}
+
+## Related Commands
+
+%s
+`
 )
 
 // GenerateDocs will generate the documentation
@@ -31,16 +100,16 @@ func GenerateDocs() error {
 	addPersistentFlags(baseCmd, cli)
 	addSubCommands(baseCmd, cli)
 
-	return GenMarkdownTreeCustom(baseCmd, docsPath)
+	return GenMarkdownTree(baseCmd, docsPath)
 }
 
-// GenMarkdownTreeCustom is the the same as GenMarkdownTree, but with linkHandler.
-func GenMarkdownTreeCustom(cmd *cobra.Command, dir string) error {
+// GenMarkdownTree is the same as GenMarkdownTree, but with linkHandler.
+func GenMarkdownTree(cmd *cobra.Command, dir string) error {
 	for _, c := range cmd.Commands() {
 		if !c.IsAvailableCommand() || c.IsAdditionalHelpTopicCommand() {
 			continue
 		}
-		if err := GenMarkdownTreeCustom(c, dir); err != nil {
+		if err := GenMarkdownTree(c, dir); err != nil {
 			return err
 		}
 	}
@@ -68,74 +137,24 @@ func GenMarkdownTreeCustom(cmd *cobra.Command, dir string) error {
 
 // GenerateHomepage creates custom markdown for the homepage.
 func GenerateHomepage(cmd *cobra.Command, w io.Writer) error {
+	templateBody := fmt.Sprintf(homepageTemplate, associatedCommandsFragment)
 
-	homepageTemplate :=
-		`---
-layout: home
----
-
-Build, manage and test your [Auth0](http://auth0.com/) integrations from the command line.
-
-## Installation
-
-Installation instructions available in [project README](https://github.com/auth0/auth0-cli#installation).
-
-## Authenticating to Your Tenant
-
-Authenticating to your Auth0 tenant is required for most functions of the CLI. It can be initiated by running:
-
-{{.LoginCommand}}
-
-There are two ways to authenticate:
-
-- **As a user** - Recommended when invoking on a personal machine or other interactive environment. Facilitated by [device authorization](https://auth0.com/docs/get-started/authentication-and-authorization-flow/device-authorization-flow) flow.
-- **As a machine** - Recommended when running on a server or non-interactive environments (ex: CI). Facilitated by [client credentials](https://auth0.com/docs/get-started/authentication-and-authorization-flow/client-credentials-flow) flow. Flags available for bypassing interactive shell.
-
-> **Warning**
-> Authenticating as a user is not supported for **private cloud** tenants. Instead, those users should authenticate with client credentials.
-
-## Available Commands
-
-%s
-`
-	var tpl bytes.Buffer
-	t := template.Must(template.New("homepageTemplate").Parse(fmt.Sprintf(homepageTemplate, associatedCommandsFragment)))
-
-	if err := t.Execute(&tpl, struct {
+	pageData := struct {
 		CommandPath        string
-		LoginCommand       string
 		AssociatedCommands []*cobra.Command
 	}{
-		LoginCommand:       wrapWithBackticks("auth0 login"),
 		CommandPath:        cmd.CommandPath(),
 		AssociatedCommands: cmd.Commands(),
-	}); err != nil {
-		return err
 	}
 
-	_, err := tpl.WriteTo(w)
-	return err
+	return GeneratePage(w, "homepageTemplate", templateBody, pageData)
 }
 
 // GenerateParentPage creates custom markdown for the parent command pages.
 func GenerateParentPage(cmd *cobra.Command, w io.Writer) error {
+	templateBody := fmt.Sprintf(parentPageTemplate, associatedCommandsFragment)
 
-	parentPageTemplate :=
-		`---
-layout: default
----
-# {{.Name}}
-
-{{.Description}}
-
-## Commands
-
-%s
-`
-	var tpl bytes.Buffer
-	t := template.Must(template.New("parentPageTemplate").Parse(fmt.Sprintf(parentPageTemplate, associatedCommandsFragment)))
-
-	err := t.Execute(&tpl, struct {
+	pageData := struct {
 		Name               string
 		Description        string
 		CommandPath        string
@@ -145,52 +164,16 @@ layout: default
 		Description:        cmd.Long,
 		CommandPath:        cmd.CommandPath(),
 		AssociatedCommands: cmd.Commands(),
-	})
-	if err != nil {
-		return err
 	}
 
-	_, err = tpl.WriteTo(w)
-	return err
+	return GeneratePage(w, "parentPageTemplate", templateBody, pageData)
 }
 
 // GenerateCommandPage creates custom markdown for the individual command pages.
 func GenerateCommandPage(cmd *cobra.Command, w io.Writer) error {
+	templateBody := fmt.Sprintf(commandPageTemplate, associatedCommandsFragment)
 
-	commandPageTemplate :=
-		`---
-layout: default
----
-# {{.Name}}
-
-{{.Description}}
-
-{{.UseLine}}
-
-{{if .HasFlags}}
-## Flags
-
-{{.Flags}}{{end}}
-
-{{if .HasInheritedFlags}}
-## InheritedFlags
-
-{{.InheritedFlags}}{{end}}
-
-## Examples
-
-{{.Examples}}
-
-## Related Commands
-
-%s
-`
-	var tpl bytes.Buffer
-	t := template.Must(template.New("commandPageTemplate").Parse(fmt.Sprintf(commandPageTemplate, associatedCommandsFragment)))
-
-	relatedCommands := cmd.Parent().Commands()
-
-	err := t.Execute(&tpl, struct {
+	pageData := struct {
 		Name               string
 		HasFlags           bool
 		Flags              string
@@ -205,22 +188,39 @@ layout: default
 		Name:               fmt.Sprintf("`%s`", cmd.CommandPath()),
 		Description:        cmd.Long,
 		HasFlags:           cmd.HasLocalFlags(),
-		Flags:              wrapWithBackticks(cmd.NonInheritedFlags().FlagUsages()),
+		Flags:              cmd.NonInheritedFlags().FlagUsages(),
 		HasInheritedFlags:  cmd.HasInheritedFlags(),
-		InheritedFlags:     wrapWithBackticks(cmd.InheritedFlags().FlagUsages()),
+		InheritedFlags:     cmd.InheritedFlags().FlagUsages(),
 		CommandPath:        cmd.CommandPath(),
-		AssociatedCommands: relatedCommands,
-		Examples:           wrapWithBackticks(cmd.Example),
-		UseLine:            wrapWithBackticks(cmd.UseLine()),
-	})
+		AssociatedCommands: cmd.Parent().Commands(),
+		Examples:           cmd.Example,
+		UseLine:            cmd.UseLine(),
+	}
+
+	return GeneratePage(w, "commandPageTemplate", templateBody, pageData)
+}
+
+func GeneratePage(w io.Writer, name, body string, data interface{}) error {
+	t, err := template.New(name).
+		Funcs(template.FuncMap{
+			"link": func(commandPath string) string {
+				return strings.ReplaceAll(commandPath, " ", "_") + ".md"
+			},
+			"wrapWithBackticks": func(body string) string {
+				body = strings.TrimSuffix(body, "\n")
+				return fmt.Sprintf("```\n%s\n```", body)
+			},
+		}).
+		Parse(body)
 	if err != nil {
 		return err
 	}
 
-	_, err = tpl.WriteTo(w)
-	return err
-}
+	var page bytes.Buffer
+	if err = t.Execute(&page, data); err != nil {
+		return err
+	}
 
-func wrapWithBackticks(body string) string {
-	return fmt.Sprintf("```\n%s\n```", body)
+	_, err = page.WriteTo(w)
+	return err
 }
