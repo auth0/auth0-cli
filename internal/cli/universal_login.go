dab0931cb81af4fa3f8f94740f6a185158023808
Sergiu Ghitea <sergiu.ghitea@okta.com>
2023-01-10 10:48:44 +0100

    Deprecate branding storybook preview

--- internal/cli/universal_login.go
+++ internal/cli/universal_login.go
@@ -1,18 +1,14 @@
 package cli
 
 import (
-	"context"
 	"errors"
 	"fmt"
 
 	"github.com/auth0/go-auth0"
 	"github.com/auth0/go-auth0/management"
 	"github.com/spf13/cobra"
-	"golang.org/x/sync/errgroup"
 
 	"github.com/auth0/auth0-cli/internal/ansi"
-	"github.com/auth0/auth0-cli/internal/branding"
-	"github.com/auth0/auth0-cli/internal/prompt"
 )
 
 var (
@@ -64,12 +60,6 @@ var (
 		AlwaysPrompt: true,
 	}
 
-	customTemplateOptions = pickerOptions{
-		{"Basic", branding.DefaultTemplate},
-		{"Login box + image", branding.ImageTemplate},
-		{"Page footers", branding.FooterTemplate},
-	}
-
 	errNotAllowed = errors.New("this feature requires at least one custom domain to be configured for the tenant")
 )
 
@@ -117,6 +107,7 @@ Once you close the window, you’ll be asked if you want to save the template. I
 	cmd.SetUsageTemplate(resourceUsageTemplate())
 	cmd.AddCommand(showBrandingTemplateCmd(cli))
 	cmd.AddCommand(updateBrandingTemplateCmd(cli))
+
 	return cmd
 }
 
@@ -130,7 +121,7 @@ func showUniversalLoginCmd(cli *cli) *cobra.Command {
   auth0 ul show
   auth0 ul show --json`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			var myBranding *management.Branding // Load app by id
+			var myBranding *management.Branding
 
 			if err := ansi.Waiting(func() error {
 				var err error
@@ -272,27 +263,23 @@ func showBrandingTemplateCmd(cli *cli) *cobra.Command {
 		Short: "Display the custom template for Universal Login",
 		Long:  "Display the custom template for the Universal Login experience.",
 		Example: `  auth0 universal-login templates show
-  auth0 ul templates show
-  auth0 ul templates show --json`,
+  auth0 ul templates show`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			var template *management.BrandingUniversalLogin // Load app by id
+			var template *management.BrandingUniversalLogin
 
-			if err := ansi.Waiting(func() error {
-				var err error
+			if err := ansi.Waiting(func() (err error) {
 				template, err = cli.api.Branding.UniversalLogin()
 				return err
 			}); err != nil {
 				return fmt.Errorf("unable to load the Universal Login template due to an unexpected error: %w", err)
 			}
 
-			cli.renderer.Heading("template")
+			cli.renderer.Heading("universal login template")
 			fmt.Println(template.GetBody())
 
 			return nil
 		},
 	}
-
-	cmd.Flags().BoolVar(&cli.json, "json", false, "Output in json format.")
 
 	return cmd
 }
@@ -306,183 +293,9 @@ func updateBrandingTemplateCmd(cli *cli) *cobra.Command {
 		Example: `  auth0 universal-login templates update
   auth0 ul templates update`,
 		RunE: func(cmd *cobra.Command, args []string) error {
-			var templateData *branding.TemplateData
-			err := ansi.Waiting(func() error {
-				var err error
-				templateData, err = cli.obtainCustomTemplateData(cmd.Context())
-				return err
-			})
-			if err != nil {
-				return err
-			}
-
-			if templateData.Body == "" {
-				if err := templateBody.Select(cmd, &templateData.Body, customTemplateOptions.labels(), nil); err != nil {
-					return err
-				}
-				templateData.Body = customTemplateOptions.getValue(templateData.Body)
-			}
-
-			err = cli.customTemplateEditorPromptWithPreview(
-				cmd,
-				&templateData.Body,
-				*templateData,
-			)
-			if err != nil {
-				return fmt.Errorf("failed to capture input from the editor: %w", err)
-			}
-
-			var confirmed bool
-			if err := prompt.AskBool("Do you want to save the template?", &confirmed, true); err != nil {
-				return fmt.Errorf("failed to capture prompt input: %w", err)
-			}
-
-			if !confirmed {
-				return nil
-			}
-
-			err = ansi.Waiting(func() error {
-				return cli.api.Branding.SetUniversalLogin(&management.BrandingUniversalLogin{
-					Body: &templateData.Body,
-				})
-			})
-
-			if err != nil {
-				return err
-			}
-
 			return nil
 		},
 	}
 
 	return cmd
-}
-
-func (cli *cli) customTemplateEditorPromptWithPreview(cmd *cobra.Command, body *string, templateData branding.TemplateData) error {
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
-
-	onInfo := func() {
-		cli.renderer.Infof("%s Once you close the editor, you'll be prompted to save your changes. To cancel, press CTRL+C.", ansi.Faint("Hint:"))
-	}
-
-	onFileCreated := func(filename string) {
-		templateData.Filename = filename
-
-		if err := branding.PreviewCustomTemplate(ctx, templateData); err != nil {
-			cli.renderer.Errorf("Unexpected error while previewing custom template: %w", err)
-		}
-	}
-
-	return templateBody.OpenEditorW(
-		cmd,
-		body,
-		templateData.Body,
-		"custom-template.*.html",
-		onInfo,
-		onFileCreated,
-	)
-}
-
-const (
-	defaultPrimaryColor    = "#0059d6"
-	defaultBackgroundColor = "#000000"
-	defaultLogoURL         = "https://cdn.auth0.com/manhattan/versions/1.2921.0/assets/badge.png"
-)
-
-func (cli *cli) obtainCustomTemplateData(ctx context.Context) (*branding.TemplateData, error) {
-	g, ctx := errgroup.WithContext(ctx)
-
-	var (
-		clients      *management.ClientList
-		brandingInfo *management.Branding
-		template     *management.BrandingUniversalLogin
-		tenant       *management.Tenant
-	)
-
-	g.Go(func() error {
-		var err error
-		domains, err := cli.api.CustomDomain.List()
-		if err != nil {
-			errStatus := err.(management.Error)
-			// 403 is a valid response for free tenants that don't have
-			// custom domains enabled
-			if errStatus != nil && errStatus.Status() == 403 {
-				return errNotAllowed
-			}
-
-			return err
-		}
-
-		for _, domain := range domains {
-			if domain.GetStatus() == "ready" {
-				return nil
-			}
-		}
-		return errNotAllowed
-	})
-
-	g.Go(func() error {
-		var err error
-		clients, err = cli.api.Client.List(management.Context(ctx))
-		return err
-	})
-
-	g.Go(func() error {
-		var err error
-		brandingInfo, err = cli.api.Branding.Read(management.Context(ctx))
-		if err != nil {
-			brandingInfo = &management.Branding{}
-		}
-
-		if brandingInfo.GetColors() == nil {
-			brandingInfo.Colors = &management.BrandingColors{
-				Primary:        auth0.String(defaultPrimaryColor),
-				PageBackground: auth0.String(defaultBackgroundColor),
-			}
-		}
-		if brandingInfo.LogoURL == nil {
-			brandingInfo.LogoURL = auth0.String(defaultLogoURL)
-		}
-
-		return nil
-	})
-
-	g.Go(func() error {
-		var err error
-		template, err = cli.api.Branding.UniversalLogin(management.Context(ctx))
-		if err != nil {
-			template = &management.BrandingUniversalLogin{Body: nil}
-		}
-
-		return nil
-	})
-
-	g.Go(func() error {
-		var err error
-		tenant, err = cli.api.Tenant.Read(management.Context(ctx))
-		return err
-	})
-
-	if err := g.Wait(); err != nil {
-		return nil, err
-	}
-
-	templateData := &branding.TemplateData{
-		PrimaryColor:    brandingInfo.GetColors().GetPrimary(),
-		BackgroundColor: brandingInfo.GetColors().GetPageBackground(),
-		LogoURL:         brandingInfo.GetLogoURL(),
-		TenantName:      tenant.GetFriendlyName(),
-		Body:            template.GetBody(),
-	}
-
-	for _, client := range clients.Clients {
-		templateData.Clients = append(templateData.Clients, branding.Client{
-			ID:      client.GetClientID(),
-			Name:    client.GetName(),
-			LogoURL: client.GetLogoURI(),
-		})
-	}
-
-	return templateData, nil
 }
