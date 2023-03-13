a2a21e7d9bcda67436c81ca034c823045fe9890b
Sergiu Ghitea <28300158+sergiught@users.noreply.github.com>
2023-03-13 12:53:26 +0100

    Fix revive lint issues

--- internal/users/users_embed.go
+++ internal/users/users_embed.go
@@ -5,15 +5,19 @@ import (
 )
 
 var (
+	// EmptyExample ...
 	//go:embed data/empty-example.json
 	EmptyExample string
 
+	// BasicExample ...
 	//go:embed data/basic-example.json
 	BasicExample string
 
+	// CustomPasswordHashExample ...
 	//go:embed data/custom-password-hash-example.json
 	CustomPasswordHashExample string
 
+	// MFAFactors ...
 	//go:embed data/mfa-factors.json
 	MFAFactors string
 )
