e99165906996ffc159b01eee7c9d68d9a8722b0d
Will Vedder <willvedd@gmail.com>
2023-03-30 08:34:33 -0400

    Combining test scripts for predictable and isolate test cases (#688)
    * More reliable tests
    
    * Fixing test case
    
    ---------
    
    Co-authored-by: Will Vedder <will.vedder@okta.com>

--- test/integration/scripts/create-log-stream-eventbridge-id.sh
+++ test/integration/scripts/create-log-stream-eventbridge-id.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
+FILE=./test/integration/identifiers/log-stream-eventbridge-id
+if [ -f "$FILE" ]; then
+    exit 0
+fi
+
 logStream=$( auth0 logs streams create eventbridge --name integration-test-eventbridge --aws-id 999999999999 --aws-region eu-west-1 --json --no-input )
 
 mkdir -p ./test/integration/identifiers
-echo "$logStream" | jq -r '.["id"]' > ./test/integration/identifiers/log-stream-eventbridge-id
+echo "$logStream" | jq -r '.["id"]' > $FILE
--- test/integration/scripts/create-log-stream-splunk-id.sh
+++ test/integration/scripts/create-log-stream-splunk-id.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
+FILE=./test/integration/identifiers/log-stream-splunk-id
+if [ -f "$FILE" ]; then
+    exit 0
+fi
+
 logStream=$( auth0 logs streams create splunk --name integration-test-splunk --domain "demo.splunk.com" --token "12a34ab5-c6d7-8901-23ef-456b7c89d0c1" --port "8088" --secure --json --no-input )
 
 mkdir -p ./test/integration/identifiers
-echo "$logStream" | jq -r '.["id"]' > ./test/integration/identifiers/log-stream-splunk-id
+echo "$logStream" | jq -r '.["id"]' > $FILE
