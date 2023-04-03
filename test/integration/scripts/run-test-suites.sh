30da61af3bd4cf2d0cd45f61762b11ff2700e2fb
Will Vedder <will.vedder@okta.com>
2023-04-03 08:38:48 -0400

    Fixing env check conditional

--- test/integration/scripts/run-test-suites.sh
+++ test/integration/scripts/run-test-suites.sh
@@ -2,8 +2,8 @@
 
 set -e
 
-if [[ -z "${AUTH0_CLI_CLIENT_DOMAIN}" || -z "${AUTH0_CLI_CLIENT_IDx}" || -z "${AUTH0_CLI_CLIENT_SECRET}"  ]]; then
-   echo "Error: AUTH0_CLI_CLIENT_DOMAIN, AUTH0_CLI_CLIENT_ID and AUTH0_CLI_CLIENT_SECRET environment variables needs to be set"
+if [[ -z "${AUTH0_CLI_CLIENT_DOMAIN}" || -z "${AUTH0_CLI_CLIENT_ID}" || -z "${AUTH0_CLI_CLIENT_SECRET}"  ]]; then
+   echo "Error: AUTH0_CLI_CLIENT_DOMAIN, AUTH0_CLI_CLIENT_ID and AUTH0_CLI_CLIENT_SECRET environment variables need to be set"
    exit 1
 fi
 
@@ -20,6 +20,6 @@ exit_code=$?
 
 bash ./test/integration/scripts/test-cleanup.sh
 
-auth0 logout $AUTH0_CLI_CLIENT_DOMAIN #Will test logout commmand
+auth0 logout $AUTH0_CLI_CLIENT_DOMAIN
 
 exit $exit_code
