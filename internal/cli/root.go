44482238f731192f2d11afdf2763b8e04f187f3c
Rita Zerrizuela <zeta@widcket.com>
2023-04-05 10:47:21 -0300

    Update internal/cli/root.go

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -44,8 +44,6 @@ func Execute() {
 		if v := recover(); v != nil {
 			err := fmt.Errorf("panic: %v", v)
 
-			// If we're in development mode, we should throw the panic for so we have less surprises.
-			// For non-developers, we'll swallow the panics.
 			if instrumentation.ReportException(err) {
 				fmt.Print(panicMessage) // If we're in development mode, we should throw the panic for so we have less surprises.
 			} else {
