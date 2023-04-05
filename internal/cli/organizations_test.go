63cf06b5540b28ea01619ab45735b7ff5cd4d6cc
Rita Zerrizuela <zeta@widcket.com>
2023-04-04 23:22:56 -0300

    Add tests for `organizationPickerOptions()`

---
+++ internal/cli/organizations_test.go
@@ -0,0 +1,92 @@
+package cli
+
+import (
+	"errors"
+	"testing"
+
+	"github.com/auth0/go-auth0/management"
+	"github.com/golang/mock/gomock"
+
+	"github.com/auth0/auth0-cli/internal/auth0"
+	"github.com/auth0/auth0-cli/internal/auth0/mock"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestOrganizationsPickerOptions(t *testing.T) {
+	tests := []struct {
+		name         string
+		organizations        []*management.Organization
+		apiError     error
+		assertOutput func(t testing.TB, options pickerOptions)
+		assertError  func(t testing.TB, err error)
+	}{
+		{
+			name: "happy path",
+			organizations: []*management.Organization{
+				{
+					ID:   auth0.String("some-id-1"),
+					Name: auth0.String("some-name-1"),
+				},
+				{
+					ID:   auth0.String("some-id-2"),
+					Name: auth0.String("some-name-2"),
+				},
+			},
+			assertOutput: func(t testing.TB, options pickerOptions) {
+				assert.Len(t, options, 2)
+				assert.Equal(t, "some-name-1 (some-id-1)", options[0].label)
+				assert.Equal(t, "some-id-1", options[0].value)
+				assert.Equal(t, "some-name-2 (some-id-2)", options[1].label)
+				assert.Equal(t, "some-id-2", options[1].value)
+			},
+			assertError: func(t testing.TB, err error) {
+				t.Fail()
+			},
+		},
+		{
+			name: "no organizations",
+			organizations: []*management.Organization{},
+			assertOutput: func(t testing.TB, options pickerOptions) {
+				t.Fail()
+			},
+			assertError: func(t testing.TB, err error) {
+				assert.ErrorContains(t, err, "There are currently no organizations.")
+			},
+		},
+		{
+			name: "API error",
+			apiError: errors.New("error"),
+			assertOutput: func(t testing.TB, options pickerOptions) {
+				t.Fail()
+			},
+			assertError: func(t testing.TB, err error) {
+				assert.Error(t, err)
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			organizationAPI := mock.NewMockOrganizationAPI(ctrl)
+			organizationAPI.EXPECT().
+				List(gomock.Any()).
+				Return(&management.OrganizationList{
+					Organizations: test.organizations} , test.apiError)
+
+			cli := &cli{
+				api: &auth0.API{Organization: organizationAPI},
+			}
+
+			options, err := cli.organizationPickerOptions()
+
+			if err != nil {
+				test.assertError(t, err)
+			} else {
+				test.assertOutput(t, options)
+			}
+		})
+	}
+}
