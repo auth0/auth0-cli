6d8082e1a439cdc81562965b84a3126e9e1b0e5b
Rita Zerrizuela <zeta@widcket.com>
2023-04-11 18:54:24 -0300

    Add tests for `userRolesToAddPickerOptions()`

--- internal/cli/users_roles_test.go
+++ internal/cli/users_roles_test.go
@@ -11,19 +11,159 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
+func TestUserRolesToAddPickerOptions(t *testing.T) {
+	tests := []struct {
+		name         string
+		userID       string
+		userRoles    []*management.Role
+		allRoles     []*management.Role
+		userAPIError error
+		roleAPIError error
+		assertOutput func(t testing.TB, options []string)
+		assertError  func(t testing.TB, err error)
+	}{
+		{
+			name: "happy path",
+			userRoles: []*management.Role{
+				{
+					ID:   auth0.String("some-id-1"),
+					Name: auth0.String("some-name-1"),
+				},
+				{
+					ID:   auth0.String("some-id-2"),
+					Name: auth0.String("some-name-2"),
+				},
+			},
+			allRoles: []*management.Role{
+				{
+					ID:   auth0.String("some-id-1"),
+					Name: auth0.String("some-name-1"),
+				},
+				{
+					ID:   auth0.String("some-id-2"),
+					Name: auth0.String("some-name-2"),
+				},
+				{
+					ID:   auth0.String("some-id-3"),
+					Name: auth0.String("some-name-3"),
+				},
+				{
+					ID:   auth0.String("some-id-4"),
+					Name: auth0.String("some-name-4"),
+				},
+			},
+			assertOutput: func(t testing.TB, options []string) {
+				assert.Len(t, options, 2)
+				assert.Equal(t, "some-id-3 (Name: some-name-3)", options[0])
+				assert.Equal(t, "some-id-4 (Name: some-name-4)", options[1])
+			},
+			assertError: func(t testing.TB, err error) {
+				t.Fail()
+			},
+		},
+		{
+			name:  "user API error",
+			userID: "some-id",
+			userAPIError: errors.New("error"),
+			assertOutput: func(t testing.TB, options []string) {
+				t.Fail()
+			},
+			assertError: func(t testing.TB, err error) {
+				assert.ErrorContains(t, err, "Failed to find the current roles for user with ID \"some-id\": error.")
+			},
+		},
+		{
+			name:  "role API error",
+			roleAPIError: errors.New("error"),
+			assertOutput: func(t testing.TB, options []string) {
+				t.Fail()
+			},
+			assertError: func(t testing.TB, err error) {
+				assert.ErrorContains(t, err, "Failed to list all roles: error.")
+			},
+		},
+		{
+			name:  "user already has all roles assigned",
+			userID: "some-id",
+			userRoles: []*management.Role{
+				{
+					ID:   auth0.String("some-id-1"),
+					Name: auth0.String("some-name-1"),
+				},
+				{
+					ID:   auth0.String("some-id-2"),
+					Name: auth0.String("some-name-2"),
+				},
+			},
+			allRoles: []*management.Role{
+				{
+					ID:   auth0.String("some-id-1"),
+					Name: auth0.String("some-name-1"),
+				},
+				{
+					ID:   auth0.String("some-id-2"),
+					Name: auth0.String("some-name-2"),
+				},
+			},
+			assertOutput: func(t testing.TB, options []string) {
+				t.Fail()
+			},
+			assertError: func(t testing.TB, err error) {
+				assert.ErrorContains(t, err, "The user with ID \"some-id\" has all roles assigned already.")
+			},
+		},
+	}
+
+	for _, test := range tests {
+		t.Run(test.name, func(t *testing.T) {
+			ctrl := gomock.NewController(t)
+			defer ctrl.Finish()
+
+			userAPI := mock.NewMockUserAPI(ctrl)
+			userAPI.EXPECT().
+				Roles(gomock.Eq(test.userID), gomock.Any()).
+				Return(&management.RoleList{
+					Roles: test.userRoles}, test.userAPIError)
+
+			timesRolesAPIShouldBeCalled := 1
+
+			if test.allRoles == nil && test.roleAPIError == nil {
+				timesRolesAPIShouldBeCalled = 0
+			}
+
+		    roleAPI := mock.NewMockRoleAPI(ctrl)
+			roleAPI.EXPECT().
+				List(gomock.Any()).
+				Return(&management.RoleList{Roles: test.allRoles}, test.roleAPIError).
+				Times(timesRolesAPIShouldBeCalled)
+
+			cli := &cli{
+				api: &auth0.API{User: userAPI, Role: roleAPI},
+			}
+
+			options, err := userRolesToAddPickerOptions(cli, test.userID)
+
+			if err != nil {
+				test.assertError(t, err)
+			} else {
+				test.assertOutput(t, options)
+			}
+		})
+	}
+}
+
 func TestUserRolesToRemovePickerOptions(t *testing.T) {
 	tests := []struct {
 		name         string
 		userID       string
-		roles        []*management.Role
+		userRoles        []*management.Role
 		apiError     error
 		assertOutput func(t testing.TB, options []string)
 		assertError  func(t testing.TB, err error)
 	}{
 		{
 			name: "happy path",
-			userID: "",
-			roles: []*management.Role{
+			userRoles: []*management.Role{
 				{
 					ID:   auth0.String("some-id-1"),
 					Name: auth0.String("some-name-1"),
@@ -44,8 +184,7 @@ func TestUserRolesToRemovePickerOptions(t *testing.T) {
 		},
 		{
 			name:  "no roles for user",
-			userID: "",
-			roles: []*management.Role{},
+			userRoles: []*management.Role{},
 			assertOutput: func(t testing.TB, options []string) {
 				assert.Empty(t, options)
 			},
@@ -61,7 +200,7 @@ func TestUserRolesToRemovePickerOptions(t *testing.T) {
 				t.Fail()
 			},
 			assertError: func(t testing.TB, err error) {
-				assert.ErrorContains(t, err, "Failed to find the current roles for user with ID some-id: error.")
+				assert.ErrorContains(t, err, "Failed to find the current roles for user with ID \"some-id\": error.")
 			},
 		},
 	}
@@ -74,10 +213,10 @@ func TestUserRolesToRemovePickerOptions(t *testing.T) {
 			userAPI := mock.NewMockUserAPI(ctrl)
 			userAPI.EXPECT().
 				Roles(gomock.Eq(test.userID), gomock.Any()).
-				Return(&management.RoleList{ Roles: test.roles }, test.apiError)
+				Return(&management.RoleList{Roles: test.userRoles}, test.apiError)
 
 			cli := &cli{
-				api: &auth0.API{ User: userAPI },
+				api: &auth0.API{User: userAPI},
 			}
 
 			options, err := userRolesToRemovePickerOptions(cli, test.userID)
