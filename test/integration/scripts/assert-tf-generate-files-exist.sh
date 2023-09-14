d7cc3880efd3ccbb69e57980ce0b9c8040871bc7
Will Vedder <will.vedder@okta.com>
2023-09-14 15:43:19 -0400

    Adding script, updating tests

---
+++ test/integration/scripts/assert-tf-generate-files-exist.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+files=(
+    "tmp-tf-gen/auth0_generated.tf" 
+    "tmp-tf-gen/auth0_import.tf" 
+    "tmp-tf-gen/auth0_main.tf" 
+    "tmp-tf-gen/terraform" 
+    "tmp-tf-gen/.terraform.lock.hcl" 
+)
+
+has_error=false
+
+for file in "${files[@]}"; do
+    if [ -e "$file" ]; then
+        if ! [ -s "$file" ]; then
+        echo "$file exists but is empty"
+            has_error=true
+        fi
+    else
+        echo "$file does not exist"
+        has_error=true
+    fi
+done
+
+if [ "$has_error" = true ]; then
+    exit 1
+fi
