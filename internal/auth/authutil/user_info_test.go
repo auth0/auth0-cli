66252ab70613f8e2c57d309334ca47b08dda673d
Will Vedder <willvedd@gmail.com>
2023-09-20 10:55:11 -0400

    Adding custom JSON deserializer for UserInfo type (#851)
    * Adding custom JSON deserializer for UserInfo type
    
    * Adding error test case
    
    * Errorf instead of sprintf
    
    * Removing error stutter
    
    ---------
    
    Co-authored-by: Will Vedder <will.vedder@okta.com>

--- internal/auth/authutil/user_info_test.go
+++ internal/auth/authutil/user_info_test.go
@@ -16,7 +16,7 @@ func TestUserInfo(t *testing.T) {
 			assert.Equal(t, "Bearer token", r.Header.Get("authorization"))
 
 			w.Header().Set("Content-Type", "application/json")
-			io.WriteString(w, `{"name": "Joe Bloggs"}`)
+			io.WriteString(w, `{"name": "Joe Bloggs","email_verified":true}`)
 		}))
 
 		defer ts.Close()
@@ -27,6 +27,25 @@ func TestUserInfo(t *testing.T) {
 
 		assert.NoError(t, err)
 		assert.Equal(t, "Joe Bloggs", *user.Name)
+		assert.Equal(t, true, *user.EmailVerified)
+	})
+
+	t.Run("Successfully call user info endpoint with string encoded email verified field", func(t *testing.T) {
+		ts := httptest.NewTLSServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			assert.Equal(t, "Bearer token", r.Header.Get("authorization"))
+
+			w.Header().Set("Content-Type", "application/json")
+			io.WriteString(w, `{"email_verified":"true"}`)
+		}))
+
+		defer ts.Close()
+		parsedURL, err := url.Parse(ts.URL)
+		assert.NoError(t, err)
+
+		user, err := FetchUserInfo(ts.Client(), parsedURL.Host, "token")
+
+		assert.NoError(t, err)
+		assert.Equal(t, true, *user.EmailVerified)
 	})
 
 	testCases := []struct {
@@ -45,6 +64,12 @@ func TestUserInfo(t *testing.T) {
 			expect:     "cannot decode response: unexpected EOF",
 			httpStatus: http.StatusOK,
 			response:   `{ "foo": "bar" `,
+		},
+		{
+			name:       "Email verified field not string or bool",
+			expect:     "cannot decode response: email_verified field expected to be bool or string, got: float64",
+			httpStatus: http.StatusOK,
+			response:   `{ "email_verified": 0 }`,
 		},
 	}
 
