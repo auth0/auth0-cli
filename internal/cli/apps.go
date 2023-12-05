3ea19cd0ac053ba64ce9298a03b5fdc1bba7f934
Will Vedder <willvedd@gmail.com>
2023-12-05 10:54:17 -0500

    DXCDT-587: Warn if deleting client used to authenticate (#934)
    Adding warning message
    
    Co-authored-by: Will Vedder <will.vedder@okta.com>

--- internal/cli/apps.go
+++ internal/cli/apps.go
@@ -330,6 +330,9 @@ func deleteAppCmd(cli *cli) *cobra.Command {
 			}
 
 			if !cli.force && canPrompt(cmd) {
+				if tenant, _ := cli.Config.GetTenant(cli.tenant); tenant.ClientID == inputs.ID {
+					cli.renderer.Warnf("Warning: You're about to delete the client used to authenticate the CLI. If deleted, the CLI will cease to operate once the access token has expired.", inputs.ID)
+				}
 				if confirmed := prompt.Confirm("Are you sure you want to proceed?"); !confirmed {
 					return nil
 				}
