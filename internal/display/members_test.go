1526d5a131ce1f3c2f87086ae56073101659e1b2
Michael Christenson II <michael.christensonii@okta.com>
2023-12-21 10:30:39 -0500

    Pass by value in for loop, not by reference.

---
+++ internal/display/members_test.go
@@ -0,0 +1,35 @@
+package display
+
+import (
+	"bytes"
+	"encoding/json"
+	"github.com/auth0/go-auth0/management"
+	"github.com/stretchr/testify/assert"
+	"io"
+	"testing"
+)
+
+func TestRenderer_MembersList_json(t *testing.T) {
+	var input []management.OrganizationMember
+	for _, id := range []string{"1234", "5678"} {
+		nid := id
+		input = append(input, management.OrganizationMember{UserID: &nid})
+	}
+
+	var exp []interface{}
+	raw, _ := json.Marshal(input)
+	_ = json.Unmarshal(raw, &exp)
+
+	r := NewRenderer()
+	r.Format = OutputFormatJSON
+
+	out := bytes.NewBuffer([]byte{})
+	r.ResultWriter = out
+	r.MembersList(input)
+	buf, _ := io.ReadAll(out)
+
+	var got []interface{}
+	_ = json.Unmarshal(buf, &got)
+
+	assert.Equal(t, exp, got)
+}
