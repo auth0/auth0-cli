3202e62a3f8b7213a03cef452a55be16c2f2aa09
ramya18101 <anusriankisetty@gmail.com>
2025-01-22 23:25:38 +0530

    Support custom url params for test login cmd

--- internal/auth/authutil/login_test.go
+++ internal/auth/authutil/login_test.go
@@ -7,8 +7,8 @@ import (
 )
 
 func TestBuildLoginURL(t *testing.T) {
-	url, err := BuildLoginURL("cli-demo.us.auth0.com", "some-client-id", "http://localhost:8484", "some-state", "some-conn", "some-aud", "none", []string{"some-scope", "some-other-scope"})
+	url, err := BuildLoginURL("cli-demo.us.auth0.com", "some-client-id", "http://localhost:8484", "some-state", "some-conn", "some-aud", "none", []string{"some-scope", "some-other-scope"}, map[string]string{"foo": "bar", "bazz": "buzz"})
 
 	assert.NoError(t, err)
-	assert.Equal(t, url, "https://cli-demo.us.auth0.com/authorize?audience=some-aud&client_id=some-client-id&connection=some-conn&prompt=none&redirect_uri=http%3A%2F%2Flocalhost%3A8484&response_type=code&scope=some-scope+some-other-scope&state=some-state")
+	assert.Equal(t, url, "https://cli-demo.us.auth0.com/authorize?audience=some-aud&bazz=buzz&client_id=some-client-id&connection=some-conn&foo=bar&prompt=none&redirect_uri=http%3A%2F%2Flocalhost%3A8484&response_type=code&scope=some-scope+some-other-scope&state=some-state")
 }
