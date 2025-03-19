608b4aa60cb3d880c2749365b668f11bf27bc320
kushalshit27 <43465488+kushalshit27@users.noreply.github.com>
2025-03-19 17:12:25 +0530

    Add integration tests for network ACL commands and a script to fetch ACL IDs

---
+++ test/integration/scripts/get-network-acl-id.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+FILE=./test/integration/identifiers/network-acl-id
+if [ -f "$FILE" ]; then
+    cat $FILE
+    exit 0
+fi
+
+acl=$( auth0 network-acl create --description "integration-test-acl" --active true --priority 1 --rule '{"action":{"block":true},"scope":"tenant","match":{"ip_v4_cidrs":["192.168.1.0/24"]}}' --json --no-input )
+
+mkdir -p ./test/integration/identifiers
+echo "$acl" | jq -r '.["id"]' > $FILE
+cat $FILE
