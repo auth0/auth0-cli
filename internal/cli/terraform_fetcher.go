1ed83c9cf91e28d4445edb6b88dc7ec3452df0d7
Kiran Kumar <kiran.kumar@okta.com>
2026-01-28 18:11:36 +0530

    feat(prompt): remove unused custom text prompt types
    - Eliminated "async-approval-flow" from the customTextPromptTypes slice in terraform_fetcher.go.
    - This prompt type is not supported in go-auth0 and terraform provider.

--- internal/cli/terraform_fetcher.go
+++ internal/cli/terraform_fetcher.go
@@ -535,9 +535,8 @@ var customTextPromptTypes = []string{
 	"login", "login-id", "login-password", "login-email-verification", "signup", "signup-id", "signup-password", "reset-password",
 	"consent", "mfa-push", "mfa-otp", "mfa-voice", "mfa-phone", "mfa-webauthn", "mfa-sms", "mfa-email", "mfa-recovery-code", "mfa",
 	"status", "device-flow", "email-verification", "email-otp-challenge", "organizations", "invitation", "common", "email-identifier-challenge", "passkeys",
-	"login-passwordless", "phone-identifier-enrollment", "phone-identifier-challenge", "custom-form", "customized-consent", "logout", "captcha", "brute-force-protection", "async-approval-flow",
+	"login-passwordless", "phone-identifier-enrollment", "phone-identifier-challenge", "custom-form", "customized-consent", "logout", "captcha", "brute-force-protection",
 }
-
 
 func (f *promptCustomTextResourceFetcherResourceFetcher) FetchData(ctx context.Context) (importDataList, error) {
 	tenant, err := f.api.Tenant.Read(ctx)
