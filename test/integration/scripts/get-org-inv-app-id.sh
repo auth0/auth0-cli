a18641672d9ea4e94c423ffabb38e3bcefb75708
Kiran Kumar <kiran.kumar@okta.com>
2026-02-10 21:53:45 +0530

    refactor(tests): update organization invitation tests and remove unused script
    - Refactored organization invitation test cases for clarity and consistency.
    - Removed the `delete-app-id.sh` script as it was no longer needed.
    - Introduced `get-org-inv-app-id.sh` to handle organization invitation app ID creation and management.
    - Ensured that the new script enables organization support for the created app.

---
+++ test/integration/scripts/get-org-inv-app-id.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+FILE=./test/integration/identifiers/org-inv-app-id
+if [ -f "$FILE" ]; then
+    cat $FILE
+    exit 0
+fi
+
+app=$( auth0 apps create -n integration-test-org-inv-app -t native --description NewApp --json --no-input )
+
+client_id=$( echo "$app" | jq -r '.["client_id"]' )
+
+# Enable organization support to allow the app to be used for org invitations
+auth0 api patch "clients/${client_id}" --data '{"organization_usage":"allow","initiate_login_uri":"https://example.com/login"}' > /dev/null
+
+mkdir -p ./test/integration/identifiers
+echo "$client_id" > $FILE
+cat $FILE
