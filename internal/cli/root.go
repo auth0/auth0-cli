e0b61c508c00615fd4714f699299ed5a2bb87c0d
Kartik Jha <kartik.jha@okta.com>
2026-06-11 21:38:01 +0530

    fix: lint fixes

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -90,7 +90,6 @@ func buildRootCmd(cli *cli) *cobra.Command {
 
 			if cmd.CommandPath() != "auth0 ai skills post-install-hook" && !skillsSentinelExists() {
 				fmt.Fprintln(os.Stderr, skillsInstallTip)
-				// writeSkillsSentinel()
 			}
 
 			if !commandRequiresAuthentication(cmd.CommandPath()) {
