7d95eac22c817ebcf8fab0529986a15307c90a32
ramya18101 <anusriankisetty@gmail.com>
2026-08-31 08:40:22 +0530

    feat: add tenant domain to Heap command tracking properties

--- internal/cli/root.go
+++ internal/cli/root.go
@@ -379,6 +379,7 @@ func commandTrackingProperties(cli *cli) map[string]string {
 		"forced":        boolString(cli.force),
 		"agent_client":  cli.agentClientName(),
 		"is_api":        boolString(isAPICommand(cli.executedCommandPath)),
+		"tenant":        cli.tenant,
 	}
 }
 
